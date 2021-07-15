692c5a7f3b4d2606c8590dd9cb39176783adfbdb
ArielCalisaya <calisayaariel62@gmail.com>
2021-07-15 13:49:10 -0400

    husky@4 works

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 // import React from 'react';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../../styles/Home.module.css';
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -14,9 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to
-          {' '}
-          <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <div className={styles.grid}>
@@ -28,8 +26,7 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a href="/">
-          Powered by
-          {' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
