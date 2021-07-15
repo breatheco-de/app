f7dac73bb3d3498fc5e73f33a19590779ba76fd0
ArielCalisaya <calisayaariel62@gmail.com>
2021-07-15 13:55:31 -0400

    test eslint

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 // import React from 'react';
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../../styles/Home.module.css";
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../../styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -14,19 +14,22 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+          Welcome to
+              {' '}
+              <a href="https://nextjs.org">Next.js!</a>
+            </h1>
 
-        <div className={styles.grid}>
-          <a href="/" className={styles.card}>
-            <h2>Hola mundo</h2>
-          </a>
-        </div>
-      </main>
+            <div className={styles.grid}>
+              <a href="/" className={styles.card}>
+                <h2>Hola mundo</h2>
+              </a>
+            </div>
+          </main>
 
       <footer className={styles.footer}>
         <a href="/">
-          Powered by{" "}
+          Powered by
+          {' '}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
