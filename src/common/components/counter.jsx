4f09941653396c59f0b9aa3c93335801f78b8a56
ArielCalisaya <calisayaariel62@gmail.com>
2021-07-20 17:48:12 -0400

    added redux and chakra-ui

---
+++ src/common/components/counter.jsx
@@ -0,0 +1,41 @@
+import { useSelector, useDispatch } from 'react-redux';
+
+const useCounter = () => {
+  const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+  const increment = () => dispatch({
+    type: 'INCREMENT',
+  });
+  const decrement = () => dispatch({
+    type: 'DECREMENT',
+  });
+  const reset = () => dispatch({
+    type: 'RESET',
+  });
+  return {
+    count,
+    increment,
+    decrement,
+    reset,
+  };
+};
+
+const Counter = () => {
+  const {
+    count, increment, decrement, reset,
+  } = useCounter();
+  return (
+    <div>
+      <h1>
+        Count:
+        {' '}
+        <span>{count}</span>
+      </h1>
+      <button type="button" onClick={increment}>+1</button>
+      <button type="button" onClick={decrement}>-1</button>
+      <button type="button" onClick={reset}>Reset</button>
+    </div>
+  );
+};
+
+export default Counter;
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,15 +1,26 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import '../../styles/globals.css';
 import PropTypes from 'prop-types';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Provider } from 'react-redux';
+import { useStore } from '../store';
 
 function LearnApp({ Component, pageProps }) {
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />;
+  const store = useStore(pageProps.initialReduxState);
+
+  return (
+    <Provider store={store}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </Provider>
+  );
 }
 
 LearnApp.propTypes = {
-  pageProps: PropTypes.shape({}).isRequired,
+  pageProps: PropTypes.oneOfType([PropTypes.func, PropTypes.object]).isRequired,
   Component: PropTypes.elementType.isRequired,
 };
+export default LearnApp;
 
 // Component: PropTypes.oneOfType([PropTypes.func, PropTypes.object]).isRequired,
-export default LearnApp;
