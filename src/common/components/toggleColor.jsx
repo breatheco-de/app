b5722f80b6563d069bae1c09d84ef7fc54ac285d
ArielCalisaya <calisayaariel62@gmail.com>
2021-07-28 21:20:37 -0400

    added storybook example for toggleColor.jsx

--- src/common/components/toggleColor.jsx
+++ src/common/components/toggleColor.jsx
@@ -1,18 +1,26 @@
 import { Button, useColorMode, Stack } from '@chakra-ui/react';
+import PropTypes from 'prop-types';
 
-const ToggleColor = () => {
+const ToggleColor = ({ title }) => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
     <Stack py="10px">
       <Button onClick={toggleColorMode} variant="switchOutline">
-        Switch
+        {title}
         {' '}
         {colorMode === 'light' ? 'Dark' : 'Light'}
         -Mode
       </Button>
     </Stack>
   );
+};
+
+ToggleColor.propTypes = {
+  title: PropTypes.string,
+};
+ToggleColor.defaultProps = {
+  title: 'Switch',
 };
 
 export default ToggleColor;
