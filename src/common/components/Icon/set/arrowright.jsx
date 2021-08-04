2fdfc03920668c3137aed83bdf70245d45958283
amafjarkasi <amafjarkasi@gmail.com>
2021-08-04 01:12:31 +0000

    added icons, sidebar

---
+++ src/common/components/Icon/set/arrowright.jsx
@@ -0,0 +1,20 @@
+const arrowright = () => (
+  <svg
+    width="8"
+    height="14"
+    viewBox="0 0 8 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M0.999999 1L7 7L1 13"
+      stroke="#0097CF"
+      strokeWidth="2"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default arrowright;
--- src/common/components/supportSidebar.jsx
+++ src/common/components/supportSidebar.jsx
@@ -1,40 +1,73 @@
-import { Box, Heading, Center } from "@chakra-ui/react";
+import { Box, Heading, Text, Button } from "@chakra-ui/react";
+import Icon from "./Icon";
+
 // [height, weight, color]
 const supportSidebar = () => {
-  const defaultProps = "500px";
+  const defaultProps = "450px";
+  let actionButtons = [
+    {
+      buttonTitle: "title1",
+      buttonIcon: "conversation",
+      buttonFunction: () => console.log("button1"),
+    },
+    {
+      buttonTitle: "title2",
+      buttonIcon: "conversation",
+      buttonFunction: () => console.log("button2"),
+    },
+    {
+      buttonTitle: "title3",
+      buttonIcon: "conversation",
+      buttonFunction: () => console.log("button3"),
+    },
+  ];
   return (
-    <Box style={{backgroundColor: '#FFF4DC'}} width="500px" borderWidth="0px" borderRadius="lg" overflow="hidden">
-      <svg width="87" height="50" viewBox="0 0 87 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-<rect width="87" height="50" rx="9" fill="#FFB718"/>
-<rect width="87" height="22" fill="#FFB718"/>
-<path d="M38.748 28.013H37.5295L32.6555 33.0173V28.013H29V13H52.1515V21.132" stroke="white" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M42.4023 34.2683H49.1041L54.5874 39.2726V34.2683H57.0244V24.2596H42.4023V34.2683Z" stroke="white" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
-  
-      <Box p="6">
-        <Box d="flex" alignItems="baseline">
-          <Heading textAlign="center" justify="center">Don't Get Stuck</Heading>
+    <Box
+      backgroundColor="#FFF4DC"
+      width={defaultProps}
+      borderWidth="0px"
+      borderRadius="lg"
+      overflow="hidden"
+    >
+      <Box d="flex" justifyContent="center">
+        <Icon icon="sidesupport" width="300px" height="70px" />
+      </Box>
+      <Box p="4">
+        <Box d="flex" alignItems="baseline" justifyContent="center">
+          <Heading textAlign="center" justify="center">
+            Don&#39;t Get Stuck
+          </Heading>
         </Box>
 
-        <Box>
-          Price
-          <Box as="span" color="gray.600" fontSize="sm">
-            / wk
-          </Box>
+        <Box pt="3" d="flex" alignItems="baseline" justifyContent="center">
+          <Text fontSize="xl" textAlign="center">
+            Did you know you can schedule mentoring sessions any time or ask in
+            the Support Chat?
+          </Text>
         </Box>
 
-        <Box d="flex" mt="2" alignItems="center">
-          {/* {Array(5)
-            .fill("")
-            .map((_, i) => (
-              <StarIcon
-                key={i}
-                color={i < property.rating ? "teal.500" : "gray.300"}
-              />
-            ))} */}
-          <Box as="span" ml="2" color="gray.600" fontSize="sm">
-            Reviews
-          </Box>
+        <Box pt="3" display="flex" flexDirection="column" alignItems="center">
+          {actionButtons.map((button) => (
+            <Button
+              size="lg"
+              key={button.buttonTitle}
+              bg="#FFFFFF"
+              width="75%"
+              px="15px"
+              my="8px"
+              justifyContent="left"
+            >
+              <Box pr="20px">
+                <Icon icon={button.buttonIcon} width="25px" height="25px" />
+              </Box>
+              <Heading as="h3" fontSize="20px">
+                {button.buttonTitle}
+              </Heading>
+              <Box ml="auto">
+                <Icon icon="arrowright" width="25px" height="25px" />
+              </Box>
+            </Button>
+          ))}
         </Box>
       </Box>
     </Box>
