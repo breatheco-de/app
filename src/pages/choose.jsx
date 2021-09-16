059097e7d7a4ed438293b0bcfad44906cdc556c0
moldovanjason <moldovanjason@gmail.com>
2021-09-16 17:28:20 +0000

    added styling

--- src/pages/choose.jsx
+++ src/pages/choose.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
+import { ArrowForwardIcon } from '@chakra-ui/icons'
+import { Flex, Text } from '@chakra-ui/react'
 
 function Choose() {
     return (
-        <div>
-            <h1>Your programs</h1>
+        <Flex height="20vw" alignItems="center" justifyContent="" flexDirection="column" fontWeight="800">
+            <Text fontSize="6xl" >Your programs</Text>
             <p>Some text here as an example to see how it will all look together.</p>
-            <div>
+            <div flexDirection="row">
                 <div>
                     PREWORK
-                    Miami prework
                 </div>
                 <div>
-                    Launch this program
+                    Cohort: Miami Prework
+                </div>
+                <div fontColor={'blue.400'}>
+                    Launch this program <ArrowForwardIcon color={'blue.400'} w={5}/>
                 </div>
             </div>
-        </div>
+        </Flex>
     )
 }
 
