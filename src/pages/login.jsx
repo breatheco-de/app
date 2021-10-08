354e5b14f312f19b8f2266568f9d3b6fe2bc0b56
Jason Moldovan <moldovanjason@gmail.com>
2021-10-08 15:31:36 -0400

    added storybook stylings

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -14,29 +14,29 @@ import Icon from '../common/components/Icon/index';
 function login() {
   return (
     <Box display="flex" justifyContent="space-between" marginBottom="47px" margin="0px" height="100vh">
-      <Box align="center" alignContent="center" float="left" width="50%" marginTop="47px">
-        <Image src={logo} alt="" height="67px" width="67px" marginLeft="299px" marginTop="47px" />
+      <Box align="center" alignContent="center" width="365px" width="50%" marginTop="47px">
+        <Image src={logo} height="67px" width="67px"/>
         <Box display="flex" marginTop="67px" justifyContent="space-evenly" fontFamily="Lato" fontWeight="900" size="15px" borderBottom="1px solid #E5E5E5" width="365px" height="38px" marginBottom="74px">
           <Box align="center" color="#3A3A3A" borderBottom="3px solid #0097CF" height="38px" width="182px">LOG IN</Box>
           <Box color="#A4A4A4" width="182px">REGISTRATION</Box>
         </Box>
-        <Button cursor="pointer" width="100%" variant="outline" weight="700">
-          <Icon icon="github" width="16px" height="16px" />
+        <Button cursor="pointer" width="365px" variant="outline" weight="700">
+          <Icon icon="github" width="18px" height="18px" />
           <Text fontSize="13px" marginLeft="10px">LOG IN WITH GITHUB</Text>
         </Button>
-        <Box display="flex" flexDirection="row" align="center" alignContent="center" justifyContent="center" width="100%">
+        <Box display="flex" justifyContent="center" width="100%">
           <Box borderBottom="solid 1px #DADADA" width="165px" marginBottom="38px" marginRight="13px" />
-          <Box marginBottom="30px" marginTop="30px" color="#C4C4C4" fontSize="15px" fontFamily="lato" fontWeight="400">or</Box>
+          <Box marginBottom="30px" marginTop="30px" color="gray.default" fontSize="m">or</Box>
           <Box borderBottom="solid 1px #DADADA" width="165px" marginBottom="38px" marginLeft="14px" />
         </Box>
         <FormControl id="email" marginBottom="35px" borderRadius="3px">
-          <Input type="email" placeholder="Email" height="50px" width="365px" autoComplete="off" borderColor="#A4A4A4" />
+          <Input type="email" placeholder="Email" height="50px" width="365px" autoComplete="off" borderColor="gray.default" />
         </FormControl>
         <FormControl id="password" borderRadius="3px">
-          <Input type="password" placeholder="Password" height="50px" width="365px" borderColor="#A4A4A4" />
+          <Input type="password" placeholder="Password" height="50px" width="365px" borderColor="gray.default" />
         </FormControl>
-        <Box color="#0097CF" size="13px" fontWeight="700" font="lato" align="center" marginLeft="245px">Reset Password</Box>
-        <Button cursor="pointer" disabled="True" variant="default" marginTop="70px" width="365px" weight="700" color="#696969">Login</Button>
+        <Box color="blue.default" fontSize="m" fontWeight="700" font="lato" align="center" marginLeft="245px">Reset Password</Box>
+        <Button disabled="True" variant="default" marginTop="70px" width="365px">Login</Button>
       </Box>
       <Image src={bg} quality="100" placeholder="blur" height="100vh" width="900px" />
     </Box>
