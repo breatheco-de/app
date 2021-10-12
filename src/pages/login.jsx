12d19226166fd73d9ebee807a2ef0406c169ff69
Jason Moldovan <moldovanjason@gmail.com>
2021-10-12 14:18:31 -0400

    add Registration side of login page. added 'run tests' on the README.md. added more stlyes from chakra.

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -19,7 +19,7 @@ import Icon from '../common/components/Icon/index';
 function login() {
   return (
     <Box display="flex" justifyContent="space-between" marginBottom="47px" margin="0px" height="100vh">
-      <Box align="center" alignContent="center" width="365px" width="50%" marginTop="47px">
+      <Box align="center" alignContent="center" width="50%" marginTop="47px">
         <Image src={logo} height="67px" width="67px" />
         <Tabs marginTop="67px">
           <TabList justifyContent="center" width="365px">
@@ -34,44 +34,43 @@ function login() {
               </Button>
               <Box display="flex" justifyContent="center" width="100%">
                 <Box borderBottom="solid 1px #DADADA" width="165px" marginBottom="38px" marginRight="13px" />
-                <Box marginBottom="30px" marginTop="30px" color="gray.default" fontSize="m">or</Box>
+                <Box marginBottom="30px" marginTop="30px" color="gray.default">or</Box>
                 <Box borderBottom="solid 1px #DADADA" width="165px" marginBottom="38px" marginLeft="14px" />
               </Box>
               <FormControl id="email" marginBottom="35px" borderRadius="3px">
-                <Input type="email" placeholder="Email" height="50px" width="365px" autoComplete="off" borderColor="gray.default" />
+                <Input type="email" placeholder="Email" height="50px" width="365px" autoComplete="off" borderColor="gray.default" borderRadius="3px" />
               </FormControl>
               <FormControl id="password" borderRadius="3px">
-                <Input type="password" placeholder="Password" height="50px" width="365px" borderColor="gray.default" />
+                <Input type="password" placeholder="Password" height="50px" width="365px" borderColor="gray.default" borderRadius="3px" />
               </FormControl>
               <Box color="blue.default" fontWeight="700" align="center" marginLeft="245px">Reset Password</Box>
               <Button disabled="True" variant="default" marginTop="70px" width="365px" fontSize="l" color="gray.dark">Login</Button>
             </TabPanel>
-            <TabPanel>
-              <p>More coming soon...</p>
+            <TabPanel width="365px" padding="0px">
+              <FormControl marginTop="22px">
+                <Text color="gray.default" fontSize="sm" float="left" marginBottom="px">Full Name </Text>
+                <Input type="password" placeholder="Andrea Castillo" height="50px" width="365px" borderColor="gray.default" borderRadius="3px" />
+              </FormControl>
+              <FormControl>
+                <Text color="gray.default" fontSize="sm" float="left" marginBottom="px">Email </Text>
+                <Input type="password" placeholder="Andrea@4geeks.co" height="50px" width="365px" borderColor="gray.default" borderRadius="3px" />
+              </FormControl>
+              <FormControl>
+                <Text color="gray.default" fontSize="sm" float="left" marginBottom="px">Date of Birth </Text>
+                <Input type="password" placeholder="29 / 10 / 1990" height="50px" width="365px" borderColor="gray.default" borderRadius="3px" />
+              </FormControl>
+              <FormControl>
+                <Text color="gray.default" fontSize="sm" float="left" marginBottom="px">Password </Text>
+                <Input type="password" placeholder="***********" height="50px" width="365px" borderColor="gray.default" borderRadius="3px" />
+              </FormControl>
+              <FormControl>
+                <Text color="gray.default" fontSize="sm" float="left" marginBottom="px">Repeat Password </Text>
+                <Input type="password" placeholder="***********" height="50px" width="365px" borderColor="gray.default" borderRadius="3px" />
+              </FormControl>
+              <Button variant="default" marginTop="33px" width="365px" fontSize="l">Register</Button>
             </TabPanel>
           </TabPanels>
         </Tabs>
-        {/* <Box display="flex" marginTop="67px" justifyContent="space-evenly" fontFamily="Lato" fontWeight="900" size="15px" borderBottom="1px solid #E5E5E5" width="365px" height="38px" marginBottom="74px">
-          <Box align="center" color="#3A3A3A" borderBottom="3px solid #0097CF" height="38px" width="182px">LOG IN</Box>
-          <Box color="#A4A4A4" width="182px">REGISTRATION</Box>
-        </Box> */}
-        {/* <Button cursor="pointer" width="365px" variant="outline" weight="700">
-          <Icon icon="github" width="18px" height="18px" />
-          <Text fontSize="13px" marginLeft="10px">LOG IN WITH GITHUB</Text>
-        </Button>
-        <Box display="flex" justifyContent="center" width="100%">
-          <Box borderBottom="solid 1px #DADADA" width="165px" marginBottom="38px" marginRight="13px" />
-          <Box marginBottom="30px" marginTop="30px" color="gray.default" fontSize="m">or</Box>
-          <Box borderBottom="solid 1px #DADADA" width="165px" marginBottom="38px" marginLeft="14px" />
-        </Box>
-        <FormControl id="email" marginBottom="35px" borderRadius="3px">
-          <Input type="email" placeholder="Email" height="50px" width="365px" autoComplete="off" borderColor="gray.default" />
-        </FormControl>
-        <FormControl id="password" borderRadius="3px">
-          <Input type="password" placeholder="Password" height="50px" width="365px" borderColor="gray.default" />
-        </FormControl>
-        <Box color="blue.default" fontWeight="700" align="center" marginLeft="245px">Reset Password</Box>
-        <Button disabled="True" variant="default" marginTop="70px" width="365px" fontSize="l" color="gray.dark">Login</Button> */}
       </Box>
       <Image src={bg} quality="100" placeholder="blur" height="100vh" width="900px" />
     </Box>
