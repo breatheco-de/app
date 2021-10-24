57cd6253cb91b5af60279e81de6419983b241d78
ArielCalisaya <calisayaariel62@gmail.com>
2021-10-23 21:44:32 -0300

    starting home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,15 @@
 import Head from 'next/head';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import {
+  Image, Box, Grid, Text,
+} from '@chakra-ui/react';
 import styles from '../../styles/Home.module.css';
-import Counter from '../common/components/Counter';
-import ToggleColor from '../common/components/ToggleColor';
-import { H1 } from '../common/styledComponents/Head';
+import Heading from '../common/components/Heading';
+import FormHandlerControl from '../common/components/FormHandlerControl';
 
 export default function Home() {
-  const { t } = useTranslation(['common']);
-
+  const { t } = useTranslation(['home']);
   return (
     <div className={styles.container}>
       <Head>
@@ -16,30 +17,37 @@ export default function Home() {
         <meta name="description" content="Learn with Breatheco.de" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Grid gridTemplateColumns={{ base: 'repeat(1,1fr)', md: 'repeat(2,1fr)' }} height="100%">
+        <Box flex="1">
+          <Heading size="xl">{t('welcome')}</Heading>
+          <Text>{t('sub_title')}</Text>
 
-      {/* <Box h="10" bg="tomato">
-        <H1 type="h1" className={styles.title}>
-          {t('heading')}
-          <a href="/">Learn!</a>
-        </H1>
-      </Box>
-      <Box h="10" bg="papayawhip" /> */}
-      <main className={styles.main}>
-        <H1 type="h1" className={styles.title}>
-          {t('heading')}
-          <a href="/">Learn!</a>
-        </H1>
-
-        <Counter />
-        <ToggleColor />
-      </main>
-      {/* </Grid> */}
+          <FormHandlerControl />
+        </Box>
+        <Box
+          flex="1"
+          display="flex"
+          flexDirection="row"
+          justifyContent="center"
+          gridGap="10px"
+          pt="6%"
+        >
+          <Box display="flex" width="35%" flexDirection="column" gridGap="10px">
+            <Image src="/static/images/person-smile1.png" />
+            <Image src="/static/images/person-smile3.png" borderRadius="15px" />
+          </Box>
+          <Box display="flex" width="35%" flexDirection="column" gridGap="10px">
+            <Image src="/static/images/person-smile2.png" />
+            <Image src="/static/images/person-smile4.png" />
+          </Box>
+        </Box>
+      </Grid>
     </div>
   );
 }
 
 export const getStaticProps = async ({ locale }) => ({
   props: {
-    ...(await serverSideTranslations(locale, ['common', 'counter', 'navbar'])),
+    ...(await serverSideTranslations(locale, ['home', 'counter', 'navbar'])),
   },
 });
