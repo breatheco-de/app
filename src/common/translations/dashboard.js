1258eaf77af8bca0652b5c428a82959ad3d05765
ArielCalisaya <calisayaariel62@gmail.com>
2022-01-26 18:56:38 -0300

    solved translation issues and improvements on dashboard page

---
+++ src/common/translations/dashboard.js
@@ -0,0 +1,37 @@
+const dashboardTR = {
+  en: {
+    title: 'Your News',
+    backToChooseProgram: 'Back to choose program',
+    progressText: 'progress in the program',
+    callToAction: {
+      title: 'Today\'s lessons!',
+      description: 'Your lesson today is ',
+      button: 'Start today\'s module',
+    },
+    cohortSideBar: {
+      title: 'About your cohort',
+      cohort: 'Cohort',
+      mainTeacher: 'Main teacher',
+      assistant: 'Assistant Professors',
+      classmates: 'Your classmates',
+    },
+  },
+  es: {
+    title: 'Tus noticias',
+    backToChooseProgram: 'Volver a elegir programa',
+    progressText: 'progreso en el programa',
+    callToAction: {
+      title: 'Clase de hoy!',
+      description: 'Tu clase de hoy es ',
+      buttonText: 'Comenzar el módulo de hoy',
+    },
+    cohortSideBar: {
+      title: 'Acerca de tu cohorte',
+      cohort: 'Cohorte',
+      mainTeacher: 'Profesor principal',
+      assistant: 'Profesores asistentes',
+      classmates: 'Tus compañeros',
+    },
+  },
+};
+export default dashboardTR;
