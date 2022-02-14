000c3ce210a308ab1bbaa79233f552d4cf217c51
ArielCalisaya <calisayaariel62@gmail.com>
2022-02-14 18:25:54 -0300

    update text

--- src/pages/thank-you.jsx
+++ src/pages/thank-you.jsx
@@ -2,7 +2,8 @@ import { Box } from '@chakra-ui/react';
 
 const ThankYou = () => (
   <Box width="100%" height="90vh" textAlign="center">
-    Thank you for your Subscription!
+    You have been subscribed into the waiting list, once your user has been accepted you will
+    receive an email invitation from an academy and you will be able to log in
   </Box>
 );
 
