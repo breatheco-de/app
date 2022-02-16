260b61a54375ba2b58cd8263a3f7959c6175def0
ArielCalisaya <calisayaariel62@gmail.com>
2022-02-16 16:46:29 -0300

    added phone input in registr view

--- src/common/components/Forms/validationSchemas.js
+++ src/common/components/Forms/validationSchemas.js
@@ -1,10 +1,12 @@
 /* eslint-disable max-len */
 import * as Yup from 'yup';
+import { phone } from '../../../utils/regex';
 
 const register = Yup.object().shape({
-  first_name: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Name is required'),
-  last_name: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Name is required'),
+  first_name: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('First name is required'),
+  last_name: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Last name is required'),
   email: Yup.string().email('Invalid email').required('Email is required'),
+  phone: Yup.string().matches(phone, 'Invalid phone number').required('Phone number is required'),
   // password: Yup.string().required('Password is required'),
   // passwordConfirmation: Yup.string().required('Required').oneOf([Yup.ref('password'), null], 'Passwords must match'),
 });
