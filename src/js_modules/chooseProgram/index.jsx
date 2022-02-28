fbbc3ba3863c6339703ef93aa964d949f991da55
ArielCalisaya <calisayaariel62@gmail.com>
2022-02-27 21:15:34 -0300

    updated choose-programs

---
+++ src/js_modules/chooseProgram/index.jsx
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+// import { useRouter } from 'next/router';
+// import axios from '../../axios';
+
+import Icon from '../../common/components/Icon';
+import Text from '../../common/components/Text';
+import ProgramList from './programList';
+
+function ChooseProgram({ chooseList, handleChoose }) {
+  console.log('chooseList:::', chooseList);
+  const [showFinished, setShowFinished] = useState(false);
+
+  const programsFinished = chooseList.filter(((program) => {
+    if (program.educational_status === 'GRADUATED' || program.cohort.stage === 'ENDED') {
+      return true;
+    }
+    return false;
+  }));
+
+  const programsRemain = chooseList.filter(((program) => {
+    if (program.cohort.stage !== 'ENDED') {
+      return true;
+    }
+    return false;
+  }));
+
+  return (
+    <>
+      {programsRemain.length > 0 && programsRemain.map((item, i) => {
+        const index = i;
+        return (
+          <ProgramList
+            key={index}
+            item={item}
+            handleChoose={handleChoose}
+          />
+        );
+      })}
+
+      {
+        programsFinished.length > 0 && (
+          <>
+            <Text
+              width={['70%', '68%', '56%', '50%']}
+              display="flex"
+              padding="25px 0"
+              flexDirection="row"
+              gridGap="6px"
+              size="md"
+            >
+              {`There are ${programsFinished.length} programs you have already finished.`}
+              <Text
+                as="button"
+                size="md"
+                _focus={{
+                  boxShadow: '0 0 0 3px rgb(66 153 225 / 60%)',
+                }}
+                color="blue.default"
+                display="flex"
+                onClick={() => setShowFinished(!showFinished)}
+              >
+                {`click here to ${showFinished ? 'hide' : 'display'} them`}
+                <Icon
+                  icon="arrowDown"
+                  width="20px"
+                  height="20px"
+                  style={{ transform: showFinished ? 'rotate(180deg)' : 'rotate(0deg)' }}
+                />
+              </Text>
+            </Text>
+            {showFinished && programsFinished.map((item, i) => {
+              const index = i;
+              return (
+                <ProgramList
+                  key={index}
+                  item={item}
+                  handleChoose={handleChoose}
+                />
+              );
+            })}
+          </>
+        )
+      }
+    </>
+  );
+}
+
+ChooseProgram.propTypes = {
+  chooseList: PropTypes.arrayOf(PropTypes.object),
+  handleChoose: PropTypes.func,
+};
+ChooseProgram.defaultProps = {
+  chooseList: [],
+  handleChoose: () => {},
+};
+
+export default ChooseProgram;
--- src/pages/choose-program.jsx
+++ src/pages/choose-program.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Flex, Box, useColorModeValue } from '@chakra-ui/react';
-import ChooseProgram from '../common/components/Choose';
+import ChooseProgram from '../js_modules/chooseProgram';
 import bc from '../common/services/breathecode';
 import asPrivate from '../common/context/PrivateRouteWrapper';
 import useAuth from '../common/hooks/useAuth';
@@ -21,8 +21,27 @@ function chooseProgram() {
 
   return (
     <Flex alignItems="center" flexDirection="column">
-      <Box fontWeight={800} width={['70%', '68%', '56%', '50%']} fontSize={['40px', '50px', '55px', '70px']} marginTop="90px">Your programs</Box>
-      <Box fontWeight={400} width={['70%', '68%', '56%', '50%']} fontSize={['10px', '15px', '15px', '20px']} color={useColorModeValue('gray.600', 'gray.200')} letterSpacing="0.05em" marginBottom="49px" marginTop="36px">Some Box here as an example to see how it will all look together. more random Box because we needed a bigger example. 4Geeks Academy is a great school that gives you great tool and teachers for you to learn all about how computers work!</Box>
+      <Box
+        fontWeight={800}
+        width={['70%', '68%', '56%', '50%']}
+        fontSize={['40px', '50px', '55px', '70px']}
+        marginTop="90px"
+      >
+        Your programs
+      </Box>
+      <Box
+        fontWeight={400}
+        width={['70%', '68%', '56%', '50%']}
+        fontSize={['10px', '15px', '15px', '20px']}
+        color={useColorModeValue('gray.600', 'gray.200')}
+        letterSpacing="0.05em"
+        marginBottom="49px"
+        marginTop="36px"
+      >
+        Some Box here as an example to see how it will all look together. more random Box
+        because we needed a bigger example. 4Geeks Academy is a great school that gives you
+        great tool and teachers for you to learn all about how computers work!
+      </Box>
       <ChooseProgram chooseList={data} handleChoose={handleChoose} />
     </Flex>
   );
