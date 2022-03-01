f4a9f996de63b1e0ef1abd0c7c0f9d0765a3e251
ArielCalisaya <calisayaariel62@gmail.com>
2022-03-01 15:07:44 -0300

    added slug id to each module

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,15 @@ const isWindow = typeof window !== 'undefined';
 
 const HAVE_SESSION = isWindow ? localStorage.getItem('accessToken') !== null : false;
 
+const slugify = (str) => str
+  .toLowerCase()
+  .trim()
+  .replace(/[^\w\s-]/g, '')
+  .replace(/[\s_-]+/g, '-')
+  .replace(/^-+|-+$/g, '');
+
 export {
   isWindow,
   HAVE_SESSION,
+  slugify,
 };
