b960001a21d6818cda82824e26e5f3f5fe53096c
Ariel Calisaya <calisayaariel62@gmail.com>
2022-03-29 16:04:30 -0300

    testing redirects

--- src/pages/_middleware.js
+++ src/pages/_middleware.js
@@ -5,19 +5,21 @@ import redirects from '../lib/redirects.json';
   the diference is that we can make smart redirects with backend
   or add functional queries to the url
 */
+
+const PUBLIC_FILE = /\.(.*)$/;
+
 const stripDefaultLocale = (str) => {
   const stripped = str.replace('/default', '');
   return stripped;
 };
 
 const middleware = async (req) => {
-  const PUBLIC_FILE = /\.(.*)$/;
   const url = await req.nextUrl.clone();
 
   const {
     pathname, origin, locale, search,
-  } = req.nextUrl.clone();
-  const currentPathname = url.pathname.toLowerCase();
+  } = url;
+  const currentPathname = pathname.toLowerCase();
   const start = Date.now();
 
   // Find the redirect from the local JSON file, do note this JSON shouldn't be
@@ -31,7 +33,13 @@ const middleware = async (req) => {
   const shouldHandleLocale = !PUBLIC_FILE.test(pathname)
     && !pathname.includes('/api/')
     && !pathname.includes('/lesson/')
+    && !pathname.includes('/default/lesson/')
+    && !pathname.includes('/es/lesson/')
+    && !pathname.includes('/en/lesson/')
     && locale === 'default';
+
+  console.log('pathname.includes("/lesson/")', pathname.includes('/lesson/'));
+  console.log('shouldHandleLocale:::', shouldHandleLocale);
 
   const redirectURL = `${origin}/en${stripDefaultLocale(pathname)}${search}`;
 
