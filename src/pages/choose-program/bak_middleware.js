299a793181f3534189aadd75e710d5f4e9074f89
Ariel Calisaya <calisayaariel62@gmail.com>
2022-04-01 23:25:39 -0300

    fixing redirects

---
+++ src/pages/choose-program/bak_middleware.js
@@ -0,0 +1,10 @@
+// import { NextResponse } from 'next/server';
+// import redirectHandler from '../../lib/redirectsHandler';
+
+// const middleware = async (req) => {
+//   const token = await req.cookies.accessToken;
+
+//   return redirectHandler(req, !token, NextResponse, '/login');
+// };
+
+// export default middleware;
---
+++ src/pages/login/bak_middleware.js
@@ -0,0 +1,11 @@
+// import { NextResponse } from 'next/server';
+// import redirectHandler from '../../lib/redirectsHandler';
+
+// const middleware = async (req) => {
+//   const token = await req.cookies.accessToken;
+
+//   // handler redirect if token exists
+//   return redirectHandler(req, token, NextResponse, '/choose-program');
+// };
+
+// export default middleware;
