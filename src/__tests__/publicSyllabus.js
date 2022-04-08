a0a7d6e96d4cb039cbfa6fd90afbdabcc6d80ef4
Ariel Calisaya <calisayaariel62@gmail.com>
2022-04-08 01:12:55 -0400

    +test for publicSyllabus

---
+++ src/__tests__/publicSyllabus.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+const fail = (msg, ...params) => {
+  console.error(msg, ...params);
+  process.exit(1);
+}
+
+const publicSyllabus = async () => {
+  console.log('process.env.BREATHECODE_HOST:', process.env.BREATHECODE_HOST);
+  const resp = await axios.get(
+    "https://breathecode-test.herokuapp.com/v1/admissions/public/syllabus?slug=full-stack,web-development",
+  )
+    .then(({ data }) => {
+      if (data.length === 0) {
+        fail('ERROR: Public syllabus returned empty array: ', data)
+      }
+      return data
+    })
+    .catch((err) => {
+      fail('ERROR: Public syllabus Fetch', err.message)
+    });
+
+  console.log('Public syllabus is working fine', resp);
+}
+
+publicSyllabus();
