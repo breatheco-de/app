839f2163f73fbb7a71fb8f53d3c095f4582175cd
Alejandro Sanchez <aalejo@gmail.com>
2022-04-08 17:02:07 -0400

    Update breathecode.js

--- src/common/services/breathecode.js
+++ src/common/services/breathecode.js
@@ -68,8 +68,8 @@ const breathecode = {
   mentorship: () => {
     const url = `${host}/mentorship/academy`;
     return {
-      getService: () => axios.get(`${url}/service`),
-      getMentor: ({ serviceSlug }) => axios.get(`${url}/mentor?service=${serviceSlug}`),
+      getService: () => axios.get(`${url}/service?status=ACTIVE`),
+      getMentor: ({ serviceSlug }) => axios.get(`${url}/mentor?service=${serviceSlug}&status=ACTIVE`),
     };
   },
   lesson: (query) => {
