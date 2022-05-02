41e9bc2cfb803e8fb313541fd7ca6701eeb58360
Ariel Calisaya <calisayaariel62@gmail.com>
2022-05-02 11:40:51 -0400

    cleaning stonly integration

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,7 +4,6 @@ import '../../styles/markdown.css';
 import TagManager from 'react-gtm-module';
 import PropTypes from 'prop-types';
 import { ChakraProvider } from '@chakra-ui/react';
-import { useRouter } from 'next/router';
 import { CookiesProvider } from 'react-cookie';
 import wrapper from '../store';
 import CustomTheme from '../../styles/theme';
@@ -19,14 +18,8 @@ import '@fontsource/lato/300.css';
 import '@fontsource/lato/400.css';
 import '@fontsource/lato/700.css';
 import '@fontsource/lato/900.css';
-import { usePersistent } from '../common/hooks/usePersistent';
 
 function App({ Component, pageProps }) {
-  const [cohortSession] = usePersistent('cohortSession', {});
-  const [taskTodo] = usePersistent('taskTodo', {});
-  const router = useRouter();
-  const [stonlyScript, setStonlyScript] = useState('');
-  const [stonlyLanguage, setStonlyLanguage] = useState('');
   const { isAuthenticated } = useAuth();
   const [haveSession, setHaveSession] = useState(false);
   const HAVE_SESSION = typeof window !== 'undefined' ? localStorage.getItem('accessToken') !== null : false;
@@ -34,35 +27,6 @@ function App({ Component, pageProps }) {
   useEffect(() => {
     TagManager.initialize({ gtmId: process.env.TAG_MANAGER_KEY });
   }, []);
-
-  useEffect(() => {
-    if (cohortSession.slug !== undefined && taskTodo !== undefined) {
-      const tasksFilteredByPending = taskTodo.filter(
-        (task) => task.revision_status === 'PENDING',
-      );
-
-      setStonlyScript(`
-      console.log('stonlyTrack running')
-      stonlyTrack('identify', '${cohortSession?.bc_id}', {
-        'academy-role': '${cohortSession?.cohort_role?.toLowerCase()}',
-        'cohort-slug': '${cohortSession?.slug}',
-        'cohort-stage': 'STARTED',
-        'academy-slug': '${cohortSession?.academy?.slug}',
-        'cohort-pending-tasks': ${tasksFilteredByPending.length}
-      });
-      `);
-    }
-  }, [cohortSession]);
-
-  useEffect(() => {
-    if (cohortSession.bc_id && router.locale) {
-      setStonlyLanguage(`
-      stonlyTrack('identify', '${cohortSession?.bc_id}', {
-        'language': '${router.locale}'
-      });
-      `);
-    }
-  }, [cohortSession, router.locale]);
 
   useEffect(() => {
     // verify if accessToken exists
@@ -82,8 +46,6 @@ function App({ Component, pageProps }) {
     <>
       <Helmet
         {...pageProps}
-        stonlyScript={stonlyScript && stonlyScript}
-        stonlyLanguage={stonlyLanguage}
       />
       <CookiesProvider>
         <AuthProvider>
