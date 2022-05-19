30e7dd7f7b4d05a0eecdd52e33c75e512970e7fb
Ariel Calisaya <calisayaariel62@gmail.com>
2022-05-18 21:25:37 -0400

    removed unused comment

--- src/common/components/AttendanceModal.jsx
+++ src/common/components/AttendanceModal.jsx
@@ -44,9 +44,6 @@ const AttendanceModal = ({
   const [checked, setChecked] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const { colorMode } = useColorMode();
-  // const sortedStudents = students.sort(
-  //   (a, b) => a.user.first_name.localeCompare(b.user.first_name),
-  // );
   const toast = useToast();
 
   const commonFontColor = useColorModeValue('gray.600', 'gray.200');
