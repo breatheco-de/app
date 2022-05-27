f4575dc58d2c3f38591cd518fda41f90fb928714
Ariel Calisaya <calisayaariel62@gmail.com>
2022-05-26 21:13:01 -0400

    integration of alias endpoint and redirect

--- src/pages/interactive-coding-tutorial/[difficulty]/_middleware.js
+++ src/pages/interactive-coding-tutorial/[difficulty]/_middleware.js
@@ -1,35 +1,27 @@
 import { NextResponse } from 'next/server';
+import { redirectByAsset } from '../../../lib/redirectsHandler';
 
 const middleware = async (req) => {
-  const url = await req.nextUrl.clone();
   const { difficulty, slug } = req.page.params;
-  const userPathName = new URL(req.url).pathname;
+  const currentPathName = new URL(req.url).pathname;
+  const pathConector = `interactive-exercises/${difficulty}`;
 
-  const results = await fetch(`${process.env.BREATHECODE_HOST}/v1/registry/asset/${slug}`)
-    .then((res) => res.json())
-    .catch((err) => console.log(err));
-  const { translations } = results;
+  const alias = await fetch(`${process.env.BREATHECODE_HOST}/v1/registry/alias/redirect`);
+  const aliasList = await alias.json();
+  const redirectSlug = aliasList[slug] || slug;
 
-  if (results.status_code !== 404) {
-    if (
-      translations.es !== undefined && (
-        userPathName === `/default/interactive-coding-tutorial/${difficulty}/${translations.es}`
-        || userPathName === `/es/interactive-coding-tutorial/${difficulty}/${translations.us}`)
-    ) {
-      console.log(`Middleware: redirecting from ${userPathName} → /es/interactive-coding-tutorial/${translations.es}`);
-      return NextResponse.redirect(new URL(`/es/interactive-coding-tutorial/${difficulty}/${translations.es}`, req.url));
-    }
+  const data = await fetch(`${process.env.BREATHECODE_HOST}/v1/registry/asset/${redirectSlug}`);
+  const results = await data.json();
 
-    if (
-      translations.us !== undefined && (
-        userPathName === `/default/interactive-coding-tutorial/${difficulty}/${translations.us}`
-        || userPathName === `/en/interactive-coding-tutorial/${difficulty}/${translations.es}`)
-    ) {
-      console.log(`Middleware: redirecting from ${url.pathname} → /en/interactive-coding-tutorial/${translations.us}`);
-      return NextResponse.redirect(new URL(`/en/interactive-coding-tutorial/${difficulty}/${translations.us}`, req.url));
-    }
-  }
-  return '';
+  const pathWithoutSlug = currentPathName.slice(0, currentPathName.lastIndexOf('/'));
+  const userPathName = `${pathWithoutSlug}/${results.slug || slug}`;
+
+  const aliasRedirect = aliasList[slug] !== undefined && userPathName;
+
+  return redirectByAsset({
+    req, pathConector, results, aliasRedirect, userPathName, NextResponse,
+  });
+  // || userPathName === `/es/interactive-coding-tutorial/${difficulty}/${translations.us}`)
 };
 
 export default middleware;
