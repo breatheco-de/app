c5999ab591949d58726772bd38fefb19e637a1f8
Ariel Calisaya <calisayaariel62@gmail.com>
2022-06-20 03:04:02 -0400

    updated kpi

--- src/pages/example.jsx
+++ src/pages/example.jsx
@@ -15,20 +15,32 @@ export const getStaticProps = () => ({
 
 export default function Example() {
   const { t } = useTranslation(['common', 'counter']);
+  const randomLabels = {
+    0: 'Tomatoes',
+    1: 'Apple',
+    2: 'Pears',
+    3: 'Avocado',
+  };
+
   const [randomRumber, setRandomRumber] = useState('0');
   const [randomValue, setRandomValue] = useState(0.0001);
+  const [randomLabel, setRandomLabel] = useState('Tomatoes');
+
   const [randomRumber2, setRandomRumber2] = useState('0');
   const [randomValue2, setRandomValue2] = useState(0.0001);
+  const [randomLabel2, setRandomLabel2] = useState('Avocado');
   useEffect(() => {
     setTimeout(() => {
       const randomOperator = Math.random() < 0.5 ? '-' : '+';
       setRandomRumber(`${randomOperator}${(Math.random() * 10).toFixed(1)}`);
-      setRandomValue((Math.random() * 0.2).toFixed(4));
+      setRandomValue(((Math.random() * 13) * 12.2).toFixed(2));
+      setRandomLabel(randomLabels[Math.floor(Math.random() * 4)]);
     }, 3500);
     setTimeout(() => {
       const randomOperator = Math.random() < 0.5 ? '-' : '+';
       setRandomRumber2(`${randomOperator}${(Math.random() * 10).toFixed(1)}`);
-      setRandomValue2((Math.random() * 0.2).toFixed(4));
+      setRandomValue2(((Math.random() * 12) * 32.2).toFixed(2));
+      setRandomLabel2(randomLabels[Math.floor(Math.random() * 4)]);
     }, 4500);
   }, [randomRumber]);
 
@@ -39,11 +51,11 @@ export default function Example() {
       </H1>
 
       <Box display="grid" mt="40px" width="100%" padding={{ base: '0 4vw', md: '0 8vw' }} gridTemplateColumns="repeat(auto-fill, minmax(15rem, 1fr))" gridGap="10px">
-        <KPI label="COIN/USDT" icon="chronometer" value={randomValue} variation={randomRumber} changeWithColor />
+        <KPI label={randomLabel} unit="$" value={randomValue} variation={randomRumber} changeWithColor />
         <KPI label="student rating" icon="smile" value={8.5} max={10} />
         <KPI label="Total monthly income" unit="$" value={2000} variation="+3.7" />
         <KPI label="Mentor late arrivals" icon="running" variation="2" value={2} max={10} />
-        <KPI label="COINDOWN/USDT" icon="chronometer" value={randomValue2} variation={randomRumber2} />
+        <KPI label={randomLabel2} unit="$" value={randomValue2} variation={randomRumber2} />
         <KPI label="Overtime" icon="chronometer" value={3} max={10} variation="-4" />
         <KPI label="The student didn't arrive" icon="ghost" value={0} max={10} variation="0" />
       </Box>
