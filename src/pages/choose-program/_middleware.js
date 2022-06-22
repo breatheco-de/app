3608a963ccabb55c27b637835e28c15408cbac59
Ariel Calisaya <calisayaariel62@gmail.com>
2022-06-22 17:11:13 -0400

    created assignments page

--- src/pages/choose-program/_middleware.js
+++ src/pages/choose-program/_middleware.js
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server';
 import { redirectHandler } from '../../lib/redirectsHandler';
 
-// TODO: Delete middleware redirections when all redirections are done
 const middleware = async (req) => {
   const token = await req.cookies.accessToken;
   const queryToken = await req.nextUrl.searchParams.get('token');
