3c37cda334f1d90d36242668d4992b98ed7ec820
Ariel Calisaya <calisayaariel62@gmail.com>
2022-08-23 16:29:32 -0400

    updated next.config

--- next.config.js
+++ next.config.js
@@ -26,7 +26,7 @@ module.exports = withBundleAnalyzer(nextTranslate({
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: '/:path*',
         headers: securityHeaders,
       },
     ];
