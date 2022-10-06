d3eb5257c62e0f4b116678e0daeb637f40e27f9c
Ariel Calisaya <calisayaariel62@gmail.com>
2022-10-05 22:40:03 -0300

    fixed order of component BeforeAfterSlider

--- src/lib/markdown-example.js
+++ src/lib/markdown-example.js
@@ -26,6 +26,11 @@ This content was blocked
 - Collaboration
 - Support
 
+<before-after
+  before="https://ca-times.brightspotcdn.com/dims4/default/c3f4b96/2147483647/strip/true/crop/1970x1108+39+0/resize/1200x675!/quality/80/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F12%2Fa5%2F79e097ccf62312d18a025f22ce48%2Fhoyla-recuento-11-cosas-aman-gatos-top-001"
+  after="https://www.thesprucepets.com/thmb/ac8-PKFO6U6I6rF52bPlJDD1onM=/1471x980/filters:fill(auto,1)/GettyImages-1288261359-4016b054680e41d28451f081babd0c45.jpg"
+/>
+
 # Loooooong code block
 \`\`\`py
 from flask import Flask
