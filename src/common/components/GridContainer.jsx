7c8a00055874aba12131b4947edd65ffb4cbae14
Ariel Calisaya <calisayaariel62@gmail.com>
2022-12-20 19:43:08 -0300

    fix height bug

--- src/common/components/GridContainer.jsx
+++ src/common/components/GridContainer.jsx
@@ -1,7 +1,7 @@
 import { Box } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
 
-const GridContainer = ({ children, gridTemplateColumns, ...rest }) => {
+const GridContainer = ({ children, gridTemplateColumns, childrenStyle, ...rest }) => {
   const fraction = rest?.fraction || '0fr';
 
   return (
@@ -13,7 +13,7 @@ const GridContainer = ({ children, gridTemplateColumns, ...rest }) => {
       gridTemplateColumns={gridTemplateColumns || `${fraction} repeat(12, 1fr) ${fraction}`}
       {...rest}
     >
-      <Box display={{ base: 'block', md: 'grid' }} gridColumn={{ base: 1, md: '2 / span 12' }}>
+      <Box display={{ base: 'block', md: 'grid' }} gridColumn={{ base: 1, md: '2 / span 12' }} style={childrenStyle}>
         {children}
       </Box>
     </Box>
@@ -23,11 +23,13 @@ const GridContainer = ({ children, gridTemplateColumns, ...rest }) => {
 GridContainer.propTypes = {
   children: PropTypes.node,
   gridTemplateColumns: PropTypes.oneOfType([PropTypes.object, PropTypes.any]),
+  childrenStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.any]),
 };
 
 GridContainer.defaultProps = {
   children: null,
   gridTemplateColumns: null,
+  childrenStyle: null,
 };
 
 export default GridContainer;
