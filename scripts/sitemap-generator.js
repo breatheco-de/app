9d86f29e7fc0bd8f656fb712d42bac331bde4b03
gustavomm19 <gustavomm199@gmail.com>
2023-02-17 21:57:18 +0000

    make changes

--- scripts/sitemap-generator.js
+++ scripts/sitemap-generator.js
@@ -194,6 +194,7 @@ async function generateSitemap() {
   const projectsSitemap = sitemapTemplate(projectsCodingRoute);
   const exercisesSitemap = sitemapTemplate(exercisesRoute);
   const technologiesSitemap = sitemapTemplate([...technologyLessonsRoute, ...technologyExercisesRoute, ...technologyProjectsRoute, ...allTechnologiesRoute]);
+  //forcing changes to pr
 
   const sitemap = listOfSitemapsTemplate([
     'pages-sitemap.xml',
