917d62320eaffc7e2f9a2be7f5d8413826f7db71
gustavomm19 <gustavomm199@gmail.com>
2023-02-17 21:04:19 +0000

    add non exercise techonolies to sitemap

--- scripts/sitemap-generator.js
+++ scripts/sitemap-generator.js
@@ -172,6 +172,7 @@ async function generateSitemap() {
   const technologyLessonsRoute = generateTechnologySlug(technologyLandingPages, 'lessons/technology');
   const technologyExercisesRoute = generateTechnologySlug(technologyLandingPages, 'interactive-exercises/technology');
   const technologyProjectsRoute = generateTechnologySlug(technologyLandingPages, 'interactive-coding-tutorials/technology');
+  const allTechnologiesRoute = generateTechnologySlug(technologyLandingPages, 'technology');
 
   // excludes Nextjs files and API routes.
   const pages = await globby([
@@ -192,7 +193,7 @@ async function generateSitemap() {
   const lessonsSitemap = sitemapTemplate(lessonsRoute);
   const projectsSitemap = sitemapTemplate(projectsCodingRoute);
   const exercisesSitemap = sitemapTemplate(exercisesRoute);
-  const technologiesSitemap = sitemapTemplate([...technologyLessonsRoute, ...technologyExercisesRoute, ...technologyProjectsRoute]);
+  const technologiesSitemap = sitemapTemplate([...technologyLessonsRoute, ...technologyExercisesRoute, ...technologyProjectsRoute, ...allTechnologiesRoute]);
 
   const sitemap = listOfSitemapsTemplate([
     'pages-sitemap.xml',
