39eb97b4eab4e51af43cfbb9902df6cb0eca5812
Ariel Calisaya <calisayaariel62@gmail.com>
2023-03-17 19:48:24 -0300

    updated robot.txt

--- src/pages/api/robots.js
+++ src/pages/api/robots.js
@@ -1,5 +1,4 @@
 export default function handler(req, res) {
-  const host = req?.headers?.host;
   const vercelEnv = process.env.VERCEL_ENV;
 
   if (vercelEnv === 'production') {
@@ -8,10 +7,8 @@ Allow: /
 Sitemap: https://4geeks.com/sitemap.xml
 Host: https://4geeks.com`);
   }
-  if (vercelEnv === 'preview' || vercelEnv === 'development') {
+  if ((vercelEnv === 'preview' || vercelEnv === 'development') && vercelEnv !== 'production') {
     res.end(`User-agent: *
 Disallow: /`);
   }
-
-  res.end(`current host: ${host}, env: ${vercelEnv}`);
 }
