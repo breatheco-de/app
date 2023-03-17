022a205bb8975ced76193c949121f28c5a0a3e31
Ariel Calisaya <calisayaariel62@gmail.com>
2023-03-16 22:08:21 -0300

    seo updates

--- src/common/components/LanguageSelector.jsx
+++ src/common/components/LanguageSelector.jsx
@@ -116,7 +116,7 @@ const LanguageSelector = ({ display, translations }) => {
 };
 
 LanguageSelector.propTypes = {
-  translations: PropTypes.objectOf(PropTypes.any),
+  translations: PropTypes.oneOfType([PropTypes.objectOf(PropTypes.any), PropTypes.arrayOf(PropTypes.any)]),
   display: PropTypes.oneOfType([PropTypes.object, PropTypes.string]).isRequired,
 };
 
