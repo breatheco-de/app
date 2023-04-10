044964aa106be41b9bfbfcabdd78a4fb0996052e
Ariel Calisaya <calisayaariel62@gmail.com>
2023-04-10 14:40:51 -0400

    initial changes

--- src/common/components/MktRoadmap.jsx
+++ src/common/components/MktRoadmap.jsx
@@ -1,5 +1,5 @@
 import {
-  Box, Container, Image, TabList, Tabs,
+  Box, Image, TabList, Tabs,
 } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
 import { useRouter } from 'next/router';
@@ -10,6 +10,7 @@ import { CustomTab } from './Animated';
 import Heading from './Heading';
 import Text from './Text';
 import useStyle from '../hooks/useStyle';
+import GridContainer from './GridContainer';
 
 const MktRoadmap = ({ id, title, course, ...rest }) => {
   const [currentTabIndex, setCurrentTabIndex] = useState(0);
@@ -31,135 +32,125 @@ const MktRoadmap = ({ id, title, course, ...rest }) => {
   }, []);
 
   return data?.length > 0 && (
-    <Container
-      px="10px"
-      id={id}
-      maxW="container.xl"
-      display="flex"
-      flexDirection={{ base: 'column', md: 'row' }}
-      height="auto"
-      position="relative"
-      alignItems="center"
-      gridGap={51}
-      {...rest}
-    >
-      <Box
-        display={{ base: 'none', md: 'inherit' }}
-        position="absolute"
-        top="-90px"
-        left="300px"
+    <GridContainer id={id} width="100%" height="auto" gridGap={51} px={{ base: '10px', md: '10px' }} {...rest}>
+      <Tabs
+        index={currentTabIndex}
+        variant="unstyled"
+        display="flex"
+        flex={0.5}
+        flexDirection={{ base: 'column', md: 'row' }}
+        height={{ base: '100%', md: '528px' }}
+        mt={{ base: '40px', md: 0 }}
+        alignItems="center"
+        gridColumn="2 / span 4"
+        position="relative"
       >
-        <Icon icon="curvedLine" width="80px" height="654px" />
-      </Box>
-      <Box display="flex" flexDirection={{ base: 'column', md: 'row' }} width="100%">
-        <Tabs
-          index={currentTabIndex}
-          variant="unstyled"
-          display="flex"
-          flex={0.5}
-          flexDirection={{ base: 'column', md: 'row' }}
-          height={{ base: '100%', md: '528px' }}
-          mt={{ base: '40px', md: 0 }}
-          alignItems="center"
-        >
-          <TabList
-            position="relative"
-            display={{ base: 'none', md: 'inherit' }}
-            height="100%"
-            zIndex={99}
-          >
-            {data?.[0]?.slug && (
-              <CustomTab onClick={() => setCurrentTabIndex(0)} top="10px" left="210px" p="1rem 12px">
-                {data?.[0]?.icon_url && (
-                  <Image src={data?.[0]?.icon_url} height="35px" style={{ marginRight: '10px' }} />
-                )}
-                {data?.[0]?.short_name || data?.[0]?.name}
-              </CustomTab>
-            )}
-
-            {data?.[1]?.slug && (
-              <CustomTab onClick={() => setCurrentTabIndex(1)} top="104px" left="240px" p="1rem 12px">
-                {data?.[1]?.icon_url && (
-                  <Image src={data?.[1]?.icon_url} height="35px" style={{ marginRight: '10px' }} />
-                )}
-                {data?.[1]?.short_name || data?.[1]?.name}
-              </CustomTab>
-            )}
-            {data?.[2]?.slug && (
-              <CustomTab onClick={() => setCurrentTabIndex(2)} top="214px" left="280px" p="1rem 12px">
-                {data?.[2]?.icon_url && (
-                  <Image src={data?.[2]?.icon_url} height="35px" style={{ marginRight: '10px' }} />
-                )}
-                {data?.[2]?.short_name || data?.[2]?.name}
-              </CustomTab>
-            )}
-            {data?.[3]?.slug && (
-              <CustomTab onClick={() => setCurrentTabIndex(3)} bottom="134px" left="270px" p="1rem 12px">
-                {data?.[3]?.icon_url && (
-                  <Image src={data?.[3]?.icon_url} height="35px" style={{ marginRight: '10px' }} />
-                )}
-                {data?.[3]?.short_name || data?.[3]?.name}
-              </CustomTab>
-            )}
-            {data?.[4]?.slug && (
-              <CustomTab onClick={() => setCurrentTabIndex(4)} bottom="57px" left="255px" p="1rem 12px">
-                {data?.[4]?.icon_url && (
-                  <Image src={data?.[4]?.icon_url} height="35px" style={{ marginRight: '10px' }} />
-                )}
-                {data?.[4]?.short_name || data?.[4]?.name}
-              </CustomTab>
-            )}
-
-          </TabList>
-        </Tabs>
         <Box
-          flex={0.5}
+          display={{ base: 'none', md: 'inherit' }}
+          position="absolute"
+          top="-20px"
+          left="50px"
+        >
+          <Icon icon="curvedLine" width="80px" height="541px" />
+        </Box>
+        <TabList
+          position="relative"
+          display={{ base: 'none', md: 'inherit' }}
+          height="100%"
+          zIndex={99}
+        >
+          {data?.[0]?.slug && (
+            <CustomTab onClick={() => setCurrentTabIndex(0)} top="10px" left="0" p="1rem 12px">
+              {data?.[0]?.icon_url && (
+                <Image src={data?.[0]?.icon_url} height="35px" style={{ marginRight: '10px' }} />
+              )}
+              {data?.[0]?.short_name || data?.[0]?.name}
+            </CustomTab>
+          )}
+
+          {data?.[1]?.slug && (
+            <CustomTab onClick={() => setCurrentTabIndex(1)} top="104px" left="240px" p="1rem 12px">
+              {data?.[1]?.icon_url && (
+                <Image src={data?.[1]?.icon_url} height="35px" style={{ marginRight: '10px' }} />
+              )}
+              {data?.[1]?.short_name || data?.[1]?.name}
+            </CustomTab>
+          )}
+          {data?.[2]?.slug && (
+            <CustomTab onClick={() => setCurrentTabIndex(2)} top="214px" left="280px" p="1rem 12px">
+              {data?.[2]?.icon_url && (
+                <Image src={data?.[2]?.icon_url} height="35px" style={{ marginRight: '10px' }} />
+              )}
+              {data?.[2]?.short_name || data?.[2]?.name}
+            </CustomTab>
+          )}
+          {data?.[3]?.slug && (
+            <CustomTab onClick={() => setCurrentTabIndex(3)} bottom="134px" left="270px" p="1rem 12px">
+              {data?.[3]?.icon_url && (
+                <Image src={data?.[3]?.icon_url} height="35px" style={{ marginRight: '10px' }} />
+              )}
+              {data?.[3]?.short_name || data?.[3]?.name}
+            </CustomTab>
+          )}
+          {data?.[4]?.slug && (
+            <CustomTab onClick={() => setCurrentTabIndex(4)} bottom="57px" left="255px" p="1rem 12px">
+              {data?.[4]?.icon_url && (
+                <Image src={data?.[4]?.icon_url} height="35px" style={{ marginRight: '10px' }} />
+              )}
+              {data?.[4]?.short_name || data?.[4]?.name}
+            </CustomTab>
+          )}
+
+        </TabList>
+      </Tabs>
+      <Box
+        // flex={0.5}
+        gridColumn="6 / span 4"
+        display="flex"
+        flexDirection="column"
+        alignSelf="center"
+        gridGap="10px"
+      >
+        {title && (
+          <Heading
+            as="h2"
+            fontSize="18px"
+            fontWeight="700"
+            lineHeight="30px"
+            letterSpacing="0.05em"
+            color="blue.default"
+          >
+            {title}
+          </Heading>
+        )}
+        <Text size="26px" fontWeight="700" lineHeight="30px">
+          {data[currentTabIndex]?.name}
+        </Text>
+        <Box
           display="flex"
           flexDirection="column"
-          alignSelf="center"
-          gridGap="10px"
+          gridGap="4rem"
+          height="12rem"
+          overflowX="auto"
         >
-          {title && (
-            <Heading
-              as="h2"
-              fontSize="18px"
-              fontWeight="700"
-              lineHeight="30px"
+          {data?.[currentTabIndex]?.description && (
+            <Text
+              key={data[currentTabIndex]?.slug}
+              className="scroll-area"
+              id={`${data[currentTabIndex]?.slug}`}
+              minHeight="12rem"
+              fontSize="14px"
+              // mb="4rem"
+              fontWeight="400"
+              lineHeight="24px"
               letterSpacing="0.05em"
-              color="blue.default"
-            >
-              {title}
-            </Heading>
+              color={fontColor3}
+              dangerouslySetInnerHTML={{ __html: data?.[currentTabIndex]?.description }}
+            />
           )}
-          <Text size="26px" fontWeight="700" lineHeight="30px">
-            {data[currentTabIndex]?.name}
-          </Text>
-          <Box
-            display="flex"
-            flexDirection="column"
-            gridGap="4rem"
-            height="12rem"
-            overflowX="auto"
-          >
-            {data?.[currentTabIndex]?.description && (
-              <Text
-                key={data[currentTabIndex]?.slug}
-                className="scroll-area"
-                id={`${data[currentTabIndex]?.slug}`}
-                minHeight="12rem"
-                fontSize="14px"
-                // mb="4rem"
-                fontWeight="400"
-                lineHeight="24px"
-                letterSpacing="0.05em"
-                color={fontColor3}
-                dangerouslySetInnerHTML={{ __html: data?.[currentTabIndex]?.description }}
-              />
-            )}
-          </Box>
         </Box>
       </Box>
-    </Container>
+    </GridContainer>
   );
 };
 MktRoadmap.propTypes = {
