06a819d46cc8b415d0dbc8f81f5e8cfaa256fdeb
Ariel Calisaya <calisayaariel62@gmail.com>
2023-04-11 16:58:38 -0400

    some updates

--- .slicemachine/prismicio.d.ts
+++ .slicemachine/prismicio.d.ts
@@ -68,7 +68,7 @@ interface PageDocumentData {
  * Slice for *Page → Slice Zone*
  *
  */
-type PageDocumentDataSlicesSlice = TitleIntroductionSlice | MarkdownSlice | TwoColumnSlice | OneColumnSlice | TestimonialsSlice | ShowPricesSlice | TechnologiesSlice | PartnersSlice | RoadMapSlice | RecommendedCoursesSlice;
+type PageDocumentDataSlicesSlice = TitleIntroductionSlice | MarkdownSlice | TwoColumnSlice | OneColumnSlice | TestimonialsSlice | ShowPricesSlice | TechnologiesSlice | PartnersSlice | RoadMapSlice | RecommendedCoursesSlice | TechnologiesPillsSlice;
 /**
  * Page document from Prismic
  *
