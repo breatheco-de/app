20fd6eb9f666d6ad7dfb40a9bb28977ba5a783c8
Ariel Calisaya <calisayaariel62@gmail.com>
2023-04-24 19:46:58 -0400

    updated prismic component

--- src/common/components/MktTwoColumnSideImage.jsx
+++ src/common/components/MktTwoColumnSideImage.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import {
-  Box, Img,
+  Box, Flex, Img,
 } from '@chakra-ui/react';
 import Heading from './Heading';
 import Text from './Text';
@@ -44,8 +44,8 @@ const MktTwoColumnSideImage = ({
 }) => {
   const { fontColor2, hexColor, backgroundColor } = useStyle();
   const flexDirection = {
-    right: 'rtl',
-    left: 'ltr',
+    right: 'ltr',
+    left: 'rtl',
   };
 
   const imageProps = slice && slice?.primary?.image?.dimensions;
@@ -70,10 +70,11 @@ const MktTwoColumnSideImage = ({
   };
   const buttonColors = getButtonColors();
 
-  const getFontStyles = () => {
+  const prisimicStyles = () => {
     if (informationSize === SIZES.SMALL) {
       return {
         titleSize: '26px',
+        titleLineHeight: '1.2',
         subtitleSize: '14px',
         descriptionSize: '12px',
       };
@@ -81,6 +82,7 @@ const MktTwoColumnSideImage = ({
     if (informationSize === SIZES.MEDIUM) {
       return {
         titleSize: '26px',
+        titleLineHeight: '1.2',
         subtitleSize: '21px',
         descriptionSize: '18px',
       };
@@ -88,17 +90,20 @@ const MktTwoColumnSideImage = ({
     if (informationSize === SIZES.LARGE) {
       return {
         titleSize: '44px',
+        titleLineHeight: '54px',
         subtitleSize: '21px',
         descriptionSize: '14px',
+        padding: '34px',
       };
     }
     return {
       titleSize: '26px',
+      titleLineHeight: '1.2',
       subtitleSize: '14px',
       descriptionSize: '12px',
     };
   };
-  const fontStyle = getFontStyles();
+  const prismicStyles = prisimicStyles();
 
   return (
     <Box
@@ -121,7 +126,50 @@ const MktTwoColumnSideImage = ({
           direction: flexDirection[imagePosition],
         }}
       >
-        <Box display={{ base: 'block', md: 'grid' }} style={{ direction: 'initial' }} gridColumn="2 / span 4">
+        {/* 2 / span 4 */}
+        <Box display={{ base: 'block', md: 'grid' }} height="100%" style={{ direction: 'initial' }} gridColumn="2 / span 4" background={textBackgroundColor} padding={prismicStyles.padding} borderRadius={{ base: '0px', md: '11px' }}>
+          <Flex flexDirection="column" gridGap="16px" alignSelf="center">
+            <Heading as="h2" size={prismicStyles.titleSize} lineHeight={prismicStyles.titleLineHeight} color={titleColor}>
+              {title}
+            </Heading>
+            {subTitle && (
+              <Heading as="h4" fontSize={prismicStyles.subtitleSize} color={subtitleColor || hexColor.blueDefault}>
+                {subTitle}
+              </Heading>
+            )}
+            {slice.primary.description ? (
+              <PrismicTextComponent
+                field={slice?.primary?.description}
+              />
+            ) : (
+              <Text
+                fontSize={prismicStyles.descriptionSize}
+                lineHeight="14px"
+                margin="15px 0"
+                color={fontColor2}
+              >
+                {description}
+              </Text>
+            )}
+            {buttonUrl && (
+              <Link
+                variant={!linkButton && 'buttonDefault'}
+                color={linkButton ? hexColor.blueDefault : buttonColors.color}
+                background={linkButton ? 'transparent' : buttonColors.background}
+                textDecoration={linkButton && 'underline'}
+                fontSize="14px"
+                margin="8px 0 0 0"
+                href={buttonUrl}
+                textAlign="center"
+                display="inline-block"
+                width="fit-content"
+              >
+                {buttonLabel}
+              </Link>
+            )}
+          </Flex>
+        </Box>
+        <Box display={{ base: 'block', md: 'grid' }} style={{ direction: 'initial' }} gridColumn="6 / span 4">
           <Img
             boxSize="100%"
             margin="0 auto"
@@ -130,47 +178,8 @@ const MktTwoColumnSideImage = ({
             alt={imageAlt}
             title={imageAlt}
             borderRadius="3px"
-            // height={imageProps?.height}
             width={imageProps?.width}
           />
-        </Box>
-        <Box display={{ base: 'block', md: 'grid' }} style={{ direction: 'initial' }} gridColumn="6 / span 4" background={textBackgroundColor}>
-          {subTitle && (
-            <Heading marginBottom="15px" as="h4" fontSize={fontStyle.subtitleSize} color={subtitleColor || hexColor.blueDefault}>
-              {subTitle}
-            </Heading>
-          )}
-          <Heading as="h2" size={fontStyle.titleSize} color={titleColor}>
-            {title}
-          </Heading>
-          {slice.primary.description ? (
-            <PrismicTextComponent
-              field={slice?.primary?.description}
-            />
-          ) : (
-            <Text
-              fontSize={fontStyle.descriptionSize}
-              lineHeight="14px"
-              margin="15px 0"
-              color={fontColor2}
-            >
-              {description}
-            </Text>
-          )}
-          {buttonUrl && (
-            <Link
-              variant={!linkButton && 'buttonDefault'}
-              color={linkButton ? hexColor.blueDefault : buttonColors.color}
-              background={buttonColors.background}
-              textDecoration={linkButton && 'underline'}
-              href={buttonUrl}
-              textAlign="center"
-              display="inline-block"
-              width="fit-content"
-            >
-              {buttonLabel}
-            </Link>
-          )}
         </Box>
       </GridContainer>
     </Box>
