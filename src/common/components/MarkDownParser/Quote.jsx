bb7759507f2cfd86130393f4e5cb78949442bf2f
avokeys <alexander.ayala.palacin@gmail.com>
2023-07-16 17:36:05 +0000

    all versions styles finished

--- src/common/components/MarkDownParser/Quote.jsx
+++ src/common/components/MarkDownParser/Quote.jsx
@@ -2,9 +2,10 @@ import React, { useEffect,useState } from 'react';
 import { Box, Flex } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
 import quoteImg from '../../img/quote.png';
+import whiteQuoteImg from '../../img/white-quote.png';
 
 const Quote = ({ children, ...props }) => {
-const [version,setVersion] = useState(3)
+const [version,setVersion] = useState(4)
   const { id, ...rest } = props;
 
   const quote = children.split('--');
@@ -17,7 +18,7 @@ const [version,setVersion] = useState(3)
   if( version == 1) {
     return (
       <Flex justifyContent="center" alignItems="center" >
-            <Box {...rest} className="quote-container" display="flex" alignContent="center" width="42%">
+            <Box {...rest} className="quote-container" display="flex" alignContent="center" width="43%">
               <Box className="quote-img" bg="#EEF9FE" width="25.7%" mr="3"  padding="9px">
                 <img src={quoteImg.src} alt="quoteImg" />
               </Box>
@@ -38,7 +39,7 @@ const [version,setVersion] = useState(3)
   else if( version  == 2){
     return (      
           <Flex justifyContent="center" alignItems="center" flexDirection="column">
-            <Box {...rest} className="quote-container" display="flex" flexDirection="column" alignContent="center" width="42%">
+            <Box {...rest} className="quote-container" display="flex" flexDirection="column" alignContent="center" width="43%">
               <Box className="quote-img" width="3.5%" mr="5" mb="3">
                 <img src={quoteImg.src} alt="quoteImg" />
               </Box>
@@ -60,7 +61,7 @@ const [version,setVersion] = useState(3)
     let splitP = quote[0].split(".")
     return (
       <Flex justifyContent="center" alignItems="center" flexDirection="column">
-      <Box {...rest} className="quote-container" display="flex" flexDirection="column" alignContent="center" width="42%">
+      <Box {...rest} className="quote-container" display="flex" flexDirection="column" alignContent="center" width="47%">
         <Box className="quote-img" width="3.5%" mr="5" mb="3">
           <img src={quoteImg.src} alt="quoteImg" />
         </Box>
@@ -69,9 +70,10 @@ const [version,setVersion] = useState(3)
           
                 <Box className="quote-paragraph">
                   {splitP[0].split(" ").map((item,index)=>{
+                  
                   return(
-                  <span key={index} style={{backgroundColor:"#0097CF"}}>
-                  {item}
+                  <span key={index} style={{backgroundColor:"#0097CF", marginRight:"3px", padding: "0px 1px 2px 3px", color:"white"}}>
+                  {index == splitP[0].split(" ").length - 1 ? item + "." + " " : item + " "}
                   </span>
                   );
                   })
@@ -91,24 +93,23 @@ const [version,setVersion] = useState(3)
   }
   else if( version == 4){
     return (
-      <Box {...rest} className="quote-container" display="flex" justifyContent="center">
-        
-  
-        <Box className="quote-img" width="32px" mr="5" mt="3">
-  
-          <img src={quoteImg.src} alt="quoteImg" />
-        </Box>
-        <Box className="quote-content" width="38%">
-          <Box className="quote-paragraph">
-            {quote[0]}
-            &quot;
-          </Box>
-          <Box className="quote-author" fontSize="sm" margin="1.5%" color="yellow">
-            --
-            {quote[1]}
-          </Box>
-        </Box>
-      </Box>
+      <Flex justifyContent="center" alignItems="center" flexDirection="column">
+            <Box {...rest} className="quote-container" display="flex" flexDirection="column" alignContent="center" width="44%">
+              <Box className="quote-img" width="5.5%" bg="#0097CF" p="5px" mr="5" mb="2">
+                <img src={whiteQuoteImg.src} alt="quoteImg" />
+              </Box>
+              <Box className="quote-content">
+                      <Box className="quote-paragraph">
+                        {quote[0]}
+                        &quot;
+                      </Box>
+                      <Box className="quote-author" fontSize="xs" margin="1.5%" color="#0097CF">
+                        --
+                        {quote[1]}
+                      </Box>
+              </Box>
+            </Box>  
+          </Flex>
     );
   }
   
