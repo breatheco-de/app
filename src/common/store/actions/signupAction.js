8cb612dd6351224081b94ee1250657f6bedbeabe
ArielCalisaya <calisayaariel62@gmail.com>
2023-09-25 19:51:48 -0300

    update translations

--- src/common/store/actions/signupAction.js
+++ src/common/store/actions/signupAction.js
@@ -107,6 +107,24 @@ const useSignup = ({ disableRedirectAfterSuccess = false } = {}) => {
     payload,
   });
 
+  const freeTrialPeriod = (qty, period) => {
+    const periodValue = period?.toLowerCase();
+    const singularTranslation = {
+      day: t('common:word-connector.day'),
+      week: t('common:word-connector.week'),
+      month: t('common:word-connector.month'),
+      year: t('common:word-connector.year'),
+    };
+    const pluralTranslation = {
+      day: t('common:word-connector.days'),
+      week: t('common:word-connector.weeks'),
+      month: t('common:word-connector.months'),
+      year: t('common:word-connector.years'),
+    };
+    const periodText = qty > 1 ? pluralTranslation[periodValue] : singularTranslation[periodValue];
+    return t('signup:info.free-trial-period', { qty, period: periodText });
+  };
+
   const handlePayment = (data) => new Promise((resolve, reject) => {
     const manyInstallmentsExists = selectedPlanCheckoutData?.financing_options?.length > 0 && selectedPlanCheckoutData?.period === 'FINANCING';
     const isTtrial = ['FREE', 'TRIAL'].includes(selectedPlanCheckoutData?.type);
@@ -209,9 +227,12 @@ const useSignup = ({ disableRedirectAfterSuccess = false } = {}) => {
           ...singlePlan,
           title: singlePlan?.title ? singlePlan?.title : toCapitalize(unSlugify(String(singlePlan?.slug))),
           price: 0,
-          priceText: isTotallyFree ? 'Free' : t('free_trial'),
+          priceText: isTotallyFree ? t('free') : t('free_trial'),
           plan_id: `p-${singlePlan?.trial_duration}-trial`,
           period: isTotallyFree ? 'FREE' : singlePlan?.trial_duration_unit,
+          period_label: isTotallyFree
+            ? t('totally_free')
+            : freeTrialPeriod(singlePlan?.trial_duration, singlePlan?.trial_duration_unit),
           type: isTotallyFree ? 'FREE' : 'TRIAL',
         } : {};
 
@@ -222,6 +243,7 @@ const useSignup = ({ disableRedirectAfterSuccess = false } = {}) => {
           priceText: `$${data?.amount_per_month}`,
           plan_id: `p-${data?.amount_per_month}`,
           period: 'MONTH',
+          period_label: t('monthly'),
           type: 'PAYMENT',
         } : {};
         const quarterPlan = existsAmountPerQuarter ? {
@@ -231,6 +253,7 @@ const useSignup = ({ disableRedirectAfterSuccess = false } = {}) => {
           priceText: `$${data?.amount_per_quarter}`,
           plan_id: `p-${data?.amount_per_quarter}`,
           period: 'QUARTER',
+          period_label: t('quarterly'),
           type: 'PAYMENT',
         } : {};
         const halfPlan = existsAmountPerHalf ? {
@@ -240,6 +263,7 @@ const useSignup = ({ disableRedirectAfterSuccess = false } = {}) => {
           priceText: `$${data?.amount_per_half}`,
           plan_id: `p-${data?.amount_per_half}`,
           period: 'HALF',
+          period_label: t('half_yearly'),
           type: 'PAYMENT',
         } : {};
 
@@ -250,6 +274,7 @@ const useSignup = ({ disableRedirectAfterSuccess = false } = {}) => {
           priceText: `$${data?.amount_per_year}`,
           plan_id: `p-${data?.amount_per_year}`,
           period: 'YEAR',
+          period_label: t('yearly'),
           type: 'PAYMENT',
         } : {};
 
@@ -264,6 +289,7 @@ const useSignup = ({ disableRedirectAfterSuccess = false } = {}) => {
             priceText: `$${item?.monthly_price} x ${item?.how_many_months}`,
             plan_id: `f-${item?.monthly_price}-${item?.how_many_months}`,
             period: 'FINANCING',
+            period_label: t('financing'),
             how_many_months: item?.how_many_months,
             type: 'PAYMENT',
           });
