786c4d47576b8fb3736b59cb41e1e11a067d66c7
Tomas Gonzalez <56565994+tommygonzaleza@users.noreply.github.com>
2023-10-27 15:16:20 -0400

    Fix `==` deployment error

--- src/common/context/AuthContext.jsx
+++ src/common/context/AuthContext.jsx
@@ -178,7 +178,7 @@ function AuthProvider({ children }) {
                 first_name: data.first_name,
                 last_name: data.last_name,
                 avatar_url: data.profile?.avatar_url || data.github?.avatar_url,
-                language: data.profile?.settings?.lang == "us" ? "en" : data.profile?.settings?.lang,
+                language: data.profile?.settings?.lang === "us" ? "en" : data.profile?.settings?.lang,
               },
             });
             if (data.github) {
