bf491004e0546a4da2a6ce236a4311406654e697
ArielCalisaya <calisayaariel62@gmail.com>
2023-11-08 21:24:53 -0300

    fix filter for search

--- src/pages/how-to/index.jsx
+++ src/pages/how-to/index.jsx
@@ -37,6 +37,7 @@ const fetchArticles = async (lang, page, query) => {
   };
   const technologies = query.techs !== '' ? query.techs : undefined;
   const video = query.withVideo === 'true' ? query.withVideo : undefined;
+  const like = query?.search;
   const querys = parseQuerys({
     asset_type: 'ARTICLE',
     visibility: 'PUBLIC',
@@ -49,6 +50,7 @@ const fetchArticles = async (lang, page, query) => {
     difficulty: difficulty[query.difficulty],
     technologies,
     video,
+    like,
   });
 
   const resp = await fetch(`${process.env.BREATHECODE_HOST}/v1/registry/asset${querys}`);
