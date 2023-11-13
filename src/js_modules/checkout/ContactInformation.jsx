d8335835f35531fc9f28d6a2cc47acc3facad063
Gustavo Márquez <47191295+gustavomm19@users.noreply.github.com>
2023-11-13 23:04:13 +0000

    reuse signup form in checkout and showonsignup

--- src/js_modules/checkout/ContactInformation.jsx
+++ src/js_modules/checkout/ContactInformation.jsx
@@ -1,31 +1,21 @@
-import { Formik, Form } from 'formik';
-import * as Yup from 'yup';
 import useTranslation from 'next-translate/useTranslation';
 import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
 import {
   Avatar,
   Tooltip,
-  Spinner,
-  InputGroup,
-  InputRightElement,
-  Box, Button, Checkbox, Flex, Image, Skeleton, useToast,
+  Box, Flex, Image, Skeleton,
 } from '@chakra-ui/react';
 import { useState, useEffect } from 'react';
 import Heading from '../../common/components/Heading';
-import bc from '../../common/services/breathecode';
-import { email as emailRe } from '../../utils/regex';
-import FieldForm from '../../common/components/Forms/FieldForm';
-import PhoneInput from '../../common/components/PhoneInput';
-import { getQueryString, getStorageItem, setStorageItem, slugToTitle, isWindow } from '../../utils';
+import Signup from '../../common/components/Forms/Signup';
+import { getStorageItem, setStorageItem, slugToTitle } from '../../utils';
 import NextChakraLink from '../../common/components/NextChakraLink';
 import useStyle from '../../common/hooks/useStyle';
-import useSession from '../../common/hooks/useSession';
 import modifyEnv from '../../../modifyEnv';
 import useSignup from '../../common/store/actions/signupAction';
 import ModalInfo from '../moduleMap/modalInfo';
 import Text from '../../common/components/Text';
-import { SILENT_CODE } from '../../lib/types';
 import Icon from '../../common/components/Icon';
 import { reportDatalayer } from '../../utils/requests';
 
@@ -34,96 +24,14 @@ function ContactInformation({
 }) {
   const BREATHECODE_HOST = modifyEnv({ queryString: 'host', env: process.env.BREATHECODE_HOST });
   const { t } = useTranslation('signup');
-  const { userSession } = useSession();
   const {
-    state, nextStep,
+    nextStep,
   } = useSignup();
-  const { stepIndex, dateProps, location } = state;
-  const plan = getQueryString('plan');
-  const planFormated = plan && encodeURIComponent(plan);
   const router = useRouter();
-  const toast = useToast();
   const [showAlreadyMember, setShowAlreadyMember] = useState(false);
-  const [isChecked, setIsChecked] = useState(false);
-  const [emailValidation, setEmailValidation] = useState({
-    valid: false,
-    loading: false,
-    error: null,
-  });
-  const [emailValue, setEmailValue] = useState(null);
   const { backgroundColor, featuredColor, hexColor } = useStyle();
   const redirectStorage = getStorageItem('redirect');
   const redirectStorageAlreadyExists = typeof redirectStorage === 'string' && redirectStorage.length > 0;
-
-  const { syllabus } = router.query;
-
-  const signupValidation = Yup.object().shape({
-    first_name: Yup.string()
-      .min(2, t('validators.short-input'))
-      .max(50, t('validators.long-input'))
-      .required(t('validators.first-name-required')),
-    last_name: Yup.string()
-      .min(2, t('validators.short-input'))
-      .max(50, t('validators.long-input'))
-      .required(t('validators.last-name-required')),
-    email: Yup.string()
-      .email(t('validators.invalid-email'))
-      .required(t('validators.email-required')),
-    phone: Yup.string(),
-    // .matches(phone, t('validators.invalid-phone')),
-    // confirm_email: Yup.string()
-    //   .oneOf([Yup.ref('email'), null], t('validators.confirm-email-not-match'))
-    //   .required(t('validators.confirm-email-required')),
-  });
-
-  const validateEmail = async (email) => {
-    try {
-      if (isWindow && email.toLowerCase().match(emailRe)) {
-        setEmailValidation({
-          valid: false,
-          loading: true,
-          error: null,
-        });
-        const cacheEmail = sessionStorage.getItem(email);
-        let result;
-        if (cacheEmail) {
-          result = JSON.parse(cacheEmail);
-        } else {
-          const response = await bc.post(`${BREATHECODE_HOST}/v1/marketing/app/validateemail`, { email });
-          result = await response.json();
-        }
-
-        sessionStorage.setItem(email, JSON.stringify(result));
-
-        if (result.status_code && result.status_code >= 400) {
-          setEmailValidation({
-            valid: false,
-            loading: false,
-            error: result.detail,
-          });
-        } else {
-          setEmailValidation({
-            valid: true,
-            loading: false,
-            error: null,
-          });
-        }
-      }
-    } catch (e) {
-      console.log(e);
-      setEmailValidation({
-        valid: false,
-        loading: false,
-        error: typeof e === 'string' ? e : e.message,
-      });
-    }
-  };
-
-  useEffect(() => {
-    if (!emailValue) return () => {};
-    const timeoutId = setTimeout(() => validateEmail(emailValue), 1000);
-    return () => clearTimeout(timeoutId);
-  }, [emailValue]);
 
   useEffect(() => {
     reportDatalayer({
@@ -133,239 +41,42 @@ function ContactInformation({
     });
   }, []);
 
-  const handleSubmit = async (actions, allValues) => {
-    try {
-      const resp = await fetch(`${BREATHECODE_HOST}/v1/auth/subscribe/`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept-Language': router?.locale || 'en',
-        },
-        body: JSON.stringify({ ...allValues, conversion_info: userSession }),
-      });
-      const data = await resp.json();
-      if (data.silent_code === SILENT_CODE.USER_EXISTS) {
-        setShowAlreadyMember(true);
-      }
-      if (resp?.status >= 400) {
-        toast({
-          position: 'top',
-          title: data?.detail,
-          status: 'error',
-          isClosable: true,
-          duration: 6000,
-        });
-      } else {
-        reportDatalayer({
-          dataLayer: {
-            event: 'sign_up',
-            method: 'native',
-            email: data.email,
-            first_name: data.first_name,
-            last_name: data.last_name,
-            plan: planFormated,
-            user_id: data.user,
-            course: allValues.course,
-            country: allValues.country,
-            city: data.city,
-            syllabus: allValues.syllabus,
-            cohort: allValues.cohort,
-            conversion_info: userSession,
-          },
-        });
-      }
-      setStorageItem('subscriptionId', data?.id);
-
-      const respPlan = await bc.payment().getPlan(planFormated);
-      const dataOfPlan = respPlan?.data;
-      if (resp.status < 400 && typeof data?.id === 'number') {
-        if (dataOfPlan?.has_waiting_list === true || data?.status === 'WAITING_LIST') {
-          setStorageItem('subscriptionId', data.id);
-          router.push('/thank-you');
-        }
-        if (data?.access_token && !dataOfPlan?.has_waiting_list) {
-          setVerifyEmailProps({
-            data: {
-              ...allValues,
-              ...data,
-            },
-            state: true,
-          });
-          router.push({
-            query: {
-              ...router.query,
-              token: data.access_token,
-            },
-          });
-          nextStep();
-        }
-      }
-
-      if (resp.status >= 400 && data?.phone) {
-        toast({
-          position: 'top',
-          title: data?.phone[0],
-          status: 'warning',
-          duration: 6000,
-          isClosable: true,
-        });
-      }
-      actions.setSubmitting(false);
-    } catch (e) {
-      console.log(e);
-      actions.setSubmitting(false);
-      toast({
-        position: 'top',
-        title: e.message,
-        status: 'error',
-        isClosable: true,
-        duration: 6000,
-      });
-    }
-  };
-
-  const isDisabled = isChecked === false && !emailValidation.loading && emailValidation.valid;
-
   return (
     <Box display="flex" height="100%" maxWidth="1336px" width="100%" margin={{ base: 'inherit', md: '1rem auto 1rem auto', '2xl': '4rem auto 4rem auto' }}>
       <Box display="flex" gridGap="10px" height="100%" width="100%">
         <Box background={featuredColor} p={{ base: '0', md: '26px 23px' }} flex={{ base: 1, md: 0.5 }} borderRadius="15px">
-          <Formik
-            initialValues={{
-              first_name: '',
-              last_name: '',
-              phone: '',
-              email: '',
-              // confirm_email: '',
-            }}
-            onSubmit={(values, actions) => {
-              const allValues = {
-                ...values,
-                phone: values?.phone.includes('undefined') ? '' : values?.phone,
-                course: courseChoosed,
-                country: location?.country,
-                cohort: dateProps?.id,
-                syllabus,
-                plan,
-                city: location?.city,
-                language: router.locale,
-              };
-
-              if (stepIndex !== 2) {
-                handleSubmit(actions, allValues);
-              }
-            }}
-            validationSchema={signupValidation}
+          <Box
+            display="flex"
+            flexDirection="column"
+            gridGap="24px"
+            padding="24px"
+            backgroundColor={hexColor.backgroundColor}
+            width="fit-content"
+            margin="3rem auto"
           >
-
-            {({ isSubmitting }) => (
-              <Form
-                style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  gridGap: '24px',
-                  padding: '24px',
-                  backgroundColor: hexColor.backgroundColor,
-                  width: 'fit-content',
-                  margin: '3rem auto',
-                }}
-              >
-                <Box display="flex" flexDirection="column" maxWidth="430px" margin="0 auto" gridGap="22px">
-                  <Box display="flex" flexDirection={{ base: 'column', sm: 'row' }}>
-                    <Heading size="18px">{t('about-you')}</Heading>
-                    <Flex fontSize="13px" ml={{ base: '0', sm: '1rem' }} mt={{ base: '10px', sm: '0' }} width="fit-content" p="2px 8px" backgroundColor={featuredColor} alignItems="center" borderRadius="4px" gridGap="6px">
-                      {t('already-have-account')}
-                      {' '}
-                      <NextChakraLink href="/login" redirectAfterLogin={!redirectStorageAlreadyExists} fontSize="12px" variant="default">{t('login-here')}</NextChakraLink>
-                    </Flex>
-                  </Box>
-                  <Box display="flex" gridGap="18px" flexDirection={{ base: 'column', md: 'row' }}>
-                    <Box display="flex" flexDirection={{ base: 'column', sm: 'row' }} gridGap="18px" flex={1}>
-                      <FieldForm
-                        type="text"
-                        name="first_name"
-                        label={t('common:first-name')}
-                        formProps={formProps}
-                        setFormProps={setFormProps}
-                      />
-                      <FieldForm
-                        type="text"
-                        name="last_name"
-                        label={t('common:last-name')}
-                        formProps={formProps}
-                        setFormProps={setFormProps}
-                      />
-                    </Box>
-                  </Box>
-                  <Box
-                    display="flex"
-                    flex={1}
-                    flexDirection="column"
-                    fontSize="12px"
-                    color="blue.default2"
-                    gridGap="4px"
-                  >
-                    <PhoneInput
-                      inputStyle={{ height: '50px' }}
-                      setVal={setFormProps}
-                      placeholder={t('common:phone')}
-                      formData={formProps}
-                      required={false}
-                      sessionContextLocation={location}
-                    />
-                    {t('phone-info')}
-                  </Box>
-                  <Box display="flex" flexDirection={{ base: 'column', sm: 'row' }} gridGap="18px">
-                    <Box
-                      display="flex"
-                      flex={1}
-                      flexDirection="column"
-                      fontSize="12px"
-                      gridGap="4px"
-                    >
-                      <InputGroup>
-                        <FieldForm
-                          type="email"
-                          name="email"
-                          label={t('common:email')}
-                          formProps={formProps}
-                          setFormProps={setFormProps}
-                          handleOnChange={(e) => setEmailValue(e.target.value)}
-                        />
-                        <InputRightElement>
-                          {emailValidation.loading && <Spinner color={hexColor.blueDefault} />}
-                        </InputRightElement>
-                      </InputGroup>
-                      {emailValidation.loading && <Box>{t('validating-email')}</Box>}
-                      {emailValidation.valid && <Box color={hexColor.green}>{t('email-validated')}</Box>}
-                      {emailValidation.error && <Box color={hexColor.danger}>{emailValidation.error}</Box>}
-                      <Box color="blue.default2">{t('email-info')}</Box>
-                    </Box>
-                  </Box>
-                  <Checkbox size="md" spacing="8px" colorScheme="green" isChecked={isChecked} onChange={() => setIsChecked(!isChecked)}>
-                    <Text size="10px">
-                      {t('validators.termns-and-conditions-required')}
-                      {' '}
-                      <NextChakraLink variant="default" fontSize="10px" href="/privacy-policy" target="_blank">
-                        {t('common:privacy-policy')}
-                      </NextChakraLink>
-                      .
-                    </Text>
-                  </Checkbox>
-                </Box>
-                <Button
-                  width="100%"
-                  type="submit"
-                  variant="default"
-                  isDisabled={isDisabled}
-                  isLoading={isSubmitting}
-                  alignSelf="flex-end"
-                >
-                  {t('create-account')}
-                </Button>
-              </Form>
-            )}
-          </Formik>
+            <Box display="flex" flexDirection={{ base: 'column', sm: 'row' }} justifyContent="space-between">
+              <Heading size="21px">{t('about-you')}</Heading>
+              <Flex fontSize="13px" ml={{ base: '0', sm: '1rem' }} mt={{ base: '10px', sm: '0' }} width="fit-content" p="2px 8px" backgroundColor={featuredColor} alignItems="center" borderRadius="4px" gridGap="6px">
+                {t('already-have-account')}
+                {' '}
+                <NextChakraLink href="/login" redirectAfterLogin={!redirectStorageAlreadyExists} fontSize="13px" variant="default">{t('login-here')}</NextChakraLink>
+              </Flex>
+            </Box>
+            <Signup
+              showVerifyEmail={false}
+              courseChoosed={courseChoosed}
+              extraFields={['phone']}
+              formProps={formProps}
+              setFormProps={setFormProps}
+              onHandleSubmit={(data) => {
+                nextStep();
+                setVerifyEmailProps({
+                  data,
+                  state: true,
+                });
+              }}
+            />
+          </Box>
         </Box>
 
         <Flex display={{ base: 'none', md: 'flex' }} flexDirection="column" alignItems="center" flex={0.5} position="relative">
