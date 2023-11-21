04a5976cd10e159b4d70b100c0a37f3a1874ed33
ArielCalisaya <calisayaariel62@gmail.com>
2023-11-21 12:43:53 -0300

    remove title="false" warn

--- src/common/components/Helmet.jsx
+++ src/common/components/Helmet.jsx
@@ -8,7 +8,7 @@ function Helmet({
   unlisted, pathConnector, locales, publishedTime, modifiedTime,
   locale, slug, disableStaticCanonical, eventStartAt, disableHreflangs,
 }) {
-  const ogTitle = title.length > 0 ? title : '4Geeks';
+  const ogTitle = (typeof title === 'string' && title?.length > 0) ? title : '4Geeks';
   const translationsArray = translations;
   const translationsExists = translationsArray.length > 0;
   const maxCharacters = 155;
@@ -57,7 +57,7 @@ function Helmet({
 
   return (
     <Head>
-      <title>{title.length > 0 ? title : '4Geeks'}</title>
+      <title>{ogTitle}</title>
       <meta name="description" content={descriptionCleaned} />
       {unlisted === true && <meta name="robots" content="noindex" />}
       <link rel="icon" href="/4Geeks.ico" />
@@ -155,7 +155,7 @@ Helmet.propTypes = {
   type: PropTypes.string,
   twitterUser: PropTypes.string,
   unlisted: PropTypes.bool,
-  translations: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.any])),
+  translations: PropTypes.oneOfType([PropTypes.array, PropTypes.object]),
   pathConnector: PropTypes.string,
   locales: PropTypes.arrayOf(PropTypes.string),
   publishedTime: PropTypes.string,
