92215a1c5bb4b5aed78c856dfbfa2c989874924b
ArielCalisaya <calisayaariel62@gmail.com>
2024-03-07 13:24:35 -0300

    Refactor Faq component to add additional props

--- src/common/components/Faq.jsx
+++ src/common/components/Faq.jsx
@@ -12,17 +12,17 @@ import Heading from './Heading';
 import Text from './Text';
 import useStyle from '../hooks/useStyle';
 
-function Faq({ items, ...rest }) {
+function Faq({ items, hideLastBorder, headingStyle, highlightColor, acordionContainerStyle, ...rest }) {
   const { hexColor } = useStyle();
   return (
     <Box position="relative" background={hexColor.featuredColor} padding="15px" borderRadius="10px" {...rest}>
-      <Heading textAlign="center" margin="20px 0">
+      <Heading textAlign="center" margin="20px 0" {...headingStyle}>
         FAQ
       </Heading>
-      <Accordion borderRadius="15px" padding="20px" allowMultiple background={hexColor.white2}>
-        {items.map((item) => (
-          <AccordionItem key={item.label} borderTop="none" borderBottom="1px solid #DADADA">
-            <AccordionButton padding="20px 10px 10px 10px">
+      <Accordion borderRadius="15px" padding="20px" allowMultiple background={hexColor.white2} {...acordionContainerStyle}>
+        {items.map((item, i) => (
+          <AccordionItem key={item.label} borderTop="none" borderBottom={(hideLastBorder && items.length - 1 > i) ? '1px solid #DADADA' : '0px'}>
+            <AccordionButton padding="20px 10px 10px 10px" _expanded={{ color: highlightColor }}>
               <Box as="span" flex="1" textAlign="left">
                 <Heading size="sm">
                   {item.label}
@@ -31,9 +31,6 @@ function Faq({ items, ...rest }) {
               <AccordionIcon />
             </AccordionButton>
             <AccordionPanel pb={4} dangerouslySetInnerHTML={{ __html: item.answer }} />
-            {/* <AccordionPanel pb={4}>
-              {item.answer}
-            </AccordionPanel> */}
           </AccordionItem>
         ))}
       </Accordion>
@@ -43,6 +40,16 @@ function Faq({ items, ...rest }) {
 
 Faq.propTypes = {
   items: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.any])).isRequired,
+  acordionContainerStyle: PropTypes.objectOf(PropTypes.oneOfType([PropTypes.string, PropTypes.any])),
+  headingStyle: PropTypes.objectOf(PropTypes.oneOfType([PropTypes.string, PropTypes.any])),
+  hideLastBorder: PropTypes.bool,
+  highlightColor: PropTypes.string,
+};
+Faq.defaultProps = {
+  acordionContainerStyle: {},
+  headingStyle: {},
+  hideLastBorder: false,
+  highlightColor: '',
 };
 
 export default Faq;
