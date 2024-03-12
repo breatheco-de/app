755086aad1728353184bb256de90120dd3bc5b01
Tomas Gonzalez <56565994+tommygonzaleza@users.noreply.github.com>
2024-03-12 15:52:56 -0400

    Update [course_slug].jsx

--- src/pages/bootcamp/[course_slug].jsx
+++ src/pages/bootcamp/[course_slug].jsx
@@ -572,8 +572,8 @@ function Page({ data, cohortData }) {
               </Flex>
               <Flex gridGap="2rem" flexDirection={{ base: 'column', md: 'row' }}>
                 {features?.list?.length > 0 && features?.list?.map((item) => (
-                  <Flex flex={{ base: 1, md: 0.33 }} flexDirection="column" gridGap="16px" padding="16px" borderRadius="8px" color={fontColor}>
-                    <Flex gridGap="8px" alignItems="center" background={hexColor.featuredColor}>
+                  <Flex flex={{ base: 1, md: 0.33 }} flexDirection="column" gridGap="16px" padding="16px" borderRadius="8px" color={fontColor} background={hexColor.featuredColor}>
+                    <Flex gridGap="8px" alignItems="center">
                       <Icon icon={item.icon} width="40px" height="35px" color={hexColor.green} />
                       <Heading size="16px" fontWeight={700} color="currentColor" lineHeight="normal">
                         {item.title}
