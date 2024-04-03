2d8c34c43876b9e410f0578c8620251ffae893e7
Tomas Gonzalez <56565994+tommygonzaleza@users.noreply.github.com>
2024-04-03 13:29:00 -0400

    Update TabletWithForm.jsx

--- src/js_modules/projects/TabletWithForm.jsx
+++ src/js_modules/projects/TabletWithForm.jsx
@@ -386,57 +386,6 @@ Lee el archivo <a class="link" href="${asset?.readme_url}">README.md</a> y sigue
             content={cloneInstructions[lang]}
             showLineNumbers={false}
           />
-          {!hideCloneButton && (
-            <Grid templateColumns="repeat(2, 1fr)" gap={2} marginBottom="15px">
-              <GridItem w="100%">
-                <Button
-                  borderRadius="3px"
-                  width="100%"
-                  fontSize="14px"
-                  padding="0"
-                  whiteSpace="normal"
-                  variant="otuline"
-                  border="1px solid"
-                  borderColor="blue.default"
-                  fontWeight="700"
-                  color="blue.default"
-                  onClick={() => {
-                    if (typeof window !== 'undefined') {
-                      window.open(`https://gitpod.io#${asset.url}`, '_blank').focus();
-                    }
-                  }}
-                >
-                  {'  '}
-                  <Icon style={{ marginRight: '5px' }} width="22px" height="26px" icon="gitpod" color={hexColor.blueDefault} />
-                  Gitpod
-                </Button>
-              </GridItem>
-              <GridItem w="100%">
-                <Button
-                  borderRadius="3px"
-                  width="100%"
-                  fontSize="14px"
-                  padding="0"
-                  whiteSpace="normal"
-                  variant="otuline"
-                  border="1px solid"
-                  borderColor="blue.default"
-                  fontWeight="700"
-                  color="blue.default"
-                  onClick={() => {
-                    if (typeof window !== 'undefined') {
-                      window.open(`https://github.com/codespaces/new/?repo=${asset.url.replace('https://github.com/', '')}`, '_blank').focus();
-                    }
-                  }}
-                >
-                  {'  '}
-                  <Icon style={{ marginRight: '5px' }} width="22px" height="26px" icon="github" color={hexColor.blueDefault} />
-                  Github Codespaces
-                </Button>
-              </GridItem>
-            </Grid>
-          )}
-
         </SimpleModal>
         <Box px="22px" pb="0" pt="0" display={{ base: 'none', md: 'block' }}>
           <SimpleTable
