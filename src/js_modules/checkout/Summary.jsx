d6920994406fc3322445b629434c71e1f8f101e0
ArielCalisaya <calisayaariel62@gmail.com>
2024-05-30 19:07:01 -0400

    Fix the bug where the plan is a free trial and it does not redirect after a successful checkout

--- src/js_modules/checkout/Summary.jsx
+++ src/js_modules/checkout/Summary.jsx
@@ -117,18 +117,22 @@ function Summary() {
     bc.cohort().join(cohort?.id)
       .then(async (resp) => {
         const dataRequested = await resp.json();
-        if (dataRequested?.status === 'ACTIVE') {
-          redirectTocohort(cohort);
+        if (resp.status >= 400) {
+          if (resp?.status === 400) { // Already in the cohort
+            redirectTocohort(cohort);
+          } else {
+            toast({
+              position: 'top',
+              title: dataRequested?.detail,
+              status: 'error',
+              duration: 5000,
+              isClosable: true,
+            });
+            setReadyToRefetch(false);
+          }
         }
-        if (dataRequested?.status_code >= 400) {
-          toast({
-            position: 'top',
-            title: dataRequested?.detail,
-            status: 'info',
-            duration: 5000,
-            isClosable: true,
-          });
-          setReadyToRefetch(false);
+        if (dataRequested?.id) {
+          redirectTocohort(cohort);
         }
       })
       .catch(() => {
@@ -180,6 +184,11 @@ function Summary() {
             }
           });
       }, 2000);
+    } else {
+      clearInterval(interval);
+      setReadyToRefetch(false);
+      setIsSubmitting(false);
+      setTimeElapsed(0);
     }
     return () => clearInterval(interval);
   }, [readyToRefetch, timeElapsed]);
