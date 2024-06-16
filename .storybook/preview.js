44b698c8aeb03e11770e1b2022af8ddb82d5b054
ArielCalisaya <51676628+ArielCalisaya@users.noreply.github.com>
2024-06-16 15:50:36 -0400

    update import path for namespaces.json

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -9,7 +9,7 @@ import CustomTheme from '../styles/theme';
 import '../styles/globals.css';
 import '../styles/markdown.css';
 import '../styles/react-tags-input.css';
-import namespaces from '../public/generated/namespaces.json';
+import namespaces from './generated/namespaces.json';
 
 import "@fontsource/lato/100.css"
 import "@fontsource/lato/300.css"
