468bd04c1bf2ce55318f11f039e95850310f1c1d
Gustavo Márquez <47191295+gustavomm19@users.noreply.github.com>
2024-07-18 14:52:04 +0000

    change console testing

--- src/pages/workshops/[event_slug].jsx
+++ src/pages/workshops/[event_slug].jsx
@@ -357,9 +357,9 @@ function Page({ eventData, asset }) {
     ),
   ) : {};
   const existsConsumables = typeof currentConsumable?.balance?.unit === 'number' && (currentConsumable?.balance?.unit > 0 || currentConsumable?.balance?.unit === -1);
-  const hasFetchedAndNoConsumablesToUse = currentConsumable?.balance?.unit === 0 || (!isRefetching && !currentConsumable?.id && noConsumablesFound) || (noConsumablesFound && consumableEventList?.length === 0);
+  const hasFetchedAndNoConsumablesToUse = currentConsumable?.balance?.unit === 0 || (!isRefetching && !currentConsumable?.id && noConsumablesFound) || (!isRefetching && noConsumablesFound && consumableEventList?.length === 0);
   useEffect(() => console.log('isRefetching', isRefetching), [isRefetching]);
-  useEffect(() => console.log('currentConsumable', currentConsumable), [currentConsumable]);
+  // useEffect(() => console.log('currentConsumable', currentConsumable), [currentConsumable]);
   useEffect(() => console.log('noConsumablesFound', noConsumablesFound), [noConsumablesFound]);
   useEffect(() => console.log('operation', !isRefetching && !currentConsumable?.id && noConsumablesFound), [isRefetching, currentConsumable, noConsumablesFound]);
 
