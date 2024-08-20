d0b0675a012cec56c2c66be1c81751bb76008510
Gustavo Márquez <47191295+gustavomm19@users.noreply.github.com>
2024-08-20 13:01:17 +0000

    fix scroll behavior on guided experience sidebar

--- src/js_modules/syllabus/GuidedExperienceSidebar.jsx
+++ src/js_modules/syllabus/GuidedExperienceSidebar.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import {
   Box,
   Button,
@@ -23,6 +23,7 @@ import useStyle from '../../common/hooks/useStyle';
 
 function GuidedExperienceSidebar({ filteredEmptyModules, onClickAssignment, isOpen, onToggle, currentModuleIndex, handleStartDay }) {
   const { t } = useTranslation('syllabus');
+  const sidebarRef = useRef(null);
   const [moduleLoading, setModuleLoading] = useState(null);
   const [expandedIndex, setExpandedIndex] = useState(currentModuleIndex);
   const { state } = useCohortHandler();
@@ -40,6 +41,22 @@ function GuidedExperienceSidebar({ filteredEmptyModules, onClickAssignment, isOp
 
   const onExpand = (index) => {
     setExpandedIndex(index);
+    if (index > -1) {
+      const section = filteredEmptyModules[index];
+      const id = `${section.label}-${section.id}`;
+      const element = document.getElementById(id);
+
+      setTimeout(() => {
+        const elementRect = element.getBoundingClientRect();
+        const sidebarRect = sidebarRef.current.getBoundingClientRect();
+
+        const scrollTopPosition = elementRect.top - sidebarRect.top + sidebarRef.current.scrollTop;
+        sidebarRef.current.scrollTo({
+          top: scrollTopPosition,
+          behavior: 'smooth',
+        });
+      }, 500);
+    }
   };
 
   return (
@@ -47,7 +64,7 @@ function GuidedExperienceSidebar({ filteredEmptyModules, onClickAssignment, isOp
       <Box position={{ base: 'fixed', lg: Open ? 'initial' : 'fixed' }} display={Open ? 'initial' : 'none'} flex="0 0 auto" minWidth="290px" width={{ base: '74.6vw', md: '46.6vw', lg: '26.6vw' }} zIndex={{ base: 100, lg: Open ? 10 : 0 }}>
         <Box style={slide}>
           <Box padding="24px 16px" background={hexColor.blueDefault}>
-            <NextChakraLink href="/choose-program" fontSize="17px" color="white" display="flex" gap="10px" alignItems="center">
+            <NextChakraLink href={cohortSession?.selectedProgramSlug} fontSize="17px" color="white" display="flex" gap="10px" alignItems="center">
               <Icon style={{ transform: 'rotate(180deg)' }} icon="logout" color="white" />
               {t('dashboard:back-to-dashboard')}
             </NextChakraLink>
@@ -86,23 +103,25 @@ function GuidedExperienceSidebar({ filteredEmptyModules, onClickAssignment, isOp
 
           <Box
             className={`horizontal-sroll ${currentThemeValue}`}
+            ref={sidebarRef}
             height="100%"
             style={{
               overflowX: 'hidden',
               overflowY: 'auto',
             }}
           >
-            {filteredEmptyModules.length > 0 && (
+            {filteredEmptyModules.length > 0 ? (
               <Accordion index={expandedIndex} onChange={onExpand} defaultIndex={[currentModuleIndex]} allowToggle allowMultiple>
                 {filteredEmptyModules.map((section) => {
                   const currentAssignments = section.filteredModules;
                   return (
-                    <AccordionItem id={`${section.title}-${section.id}`} isDisabled={moduleLoading} key={`${section.title}-${section.id}`} border="none">
+                    <AccordionItem isDisabled={moduleLoading} key={`${section.label}-${section.id}`} border="none">
                       <Box
                         padding={{ base: '1rem', md: '1.5rem' }}
                         borderBottom={1}
                         borderStyle="solid"
                         borderColor={commonBorderColor}
+                        id={`${section.label}-${section.id}`}
                       >
 
                         <AccordionButton
@@ -140,6 +159,8 @@ function GuidedExperienceSidebar({ filteredEmptyModules, onClickAssignment, isOp
                   );
                 })}
               </Accordion>
+            ) : (
+              <Spinner color={hexColor.blueDefault} />
             )}
           </Box>
         </Box>
