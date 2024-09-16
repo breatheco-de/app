878c9e01392692940203725f05d8a78e1ddcb921
Gustavo Márquez <47191295+gustavomm19@users.noreply.github.com>
2024-09-16 10:35:35 +0000

    diminish padding in exercises view

--- src/js_modules/syllabus/ExerciseGuidedExperience.jsx
+++ src/js_modules/syllabus/ExerciseGuidedExperience.jsx
@@ -113,7 +113,7 @@ function ExerciseGuidedExperience({ currentTask, currentAsset }) {
   const repoName = urlToClone?.split('/')?.pop();
 
   return (
-    <Box className={`horizontal-sroll ${colorMode}`} overflowY="auto" borderRadius="11px" background="blue.1000" height="80vh" mb="30px" padding="32px" display="flex" flexDirection="column" justifyContent="space-between" gap="20px">
+    <Box className={`horizontal-sroll ${colorMode}`} overflowY="auto" borderRadius="11px" background="blue.1000" height="80vh" mb="30px" padding="16px" display="flex" flexDirection="column" justifyContent="space-between" gap="20px">
       <Box display="flex" gap="16px" flexDirection={{ base: 'column', md: 'row' }}>
         <Box gap="16px" width="100%" display="flex" flexDirection={{ base: 'column', md: isExerciseStated ? 'column' : 'row' }} justifyContent="space-between">
           <Box maxWidth={{ base: 'none', md: isExerciseStated ? 'none' : '40%' }}>
