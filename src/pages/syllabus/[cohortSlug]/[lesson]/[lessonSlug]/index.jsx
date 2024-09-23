37b4976242f1b79fdd5583216d0e08d263fc4b80
Gustavo Márquez <47191295+gustavomm19@users.noreply.github.com>
2024-09-23 09:04:15 +0000

    remove unnecesary manual translation

--- src/pages/syllabus/[cohortSlug]/[lesson]/[lessonSlug]/index.jsx
+++ src/pages/syllabus/[cohortSlug]/[lesson]/[lessonSlug]/index.jsx
@@ -545,15 +545,12 @@ function SyllabusContent() {
     answer: 'https://assessment.4geeks.com/quiz',
   };
   const shareLink = currentTask ? `${pathConnector[lesson]}/${currentTask.associated_slug}` : '';
-  const shareSocialMessage = {
-    en: `I just finished coding ${currentTask?.title} at 4geeks.com`,
-    es: `Acabo de terminar de programar ${currentTask?.title} en 4geeks.com`,
-  };
+
   const socials = [
     {
       name: 'twitter',
       label: 'Twitter',
-      href: `https://twitter.com/share?url=&text=${encodeURIComponent(shareSocialMessage[router.locale])} %23100DaysOfCode%0A%0A${shareLink}`,
+      href: `https://twitter.com/share?url=&text=${encodeURIComponent(t('share-social-message', { title: currentTask?.title }))} %23100DaysOfCode%0A%0A${shareLink}`,
       color: '#1DA1F2',
     },
     {
