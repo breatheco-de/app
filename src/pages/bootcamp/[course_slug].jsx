43d434e2bc302a7baf13b1f23ae3d25394a7977f
lumi-tip <informacionlmdv@gmail.com>
2024-10-01 17:52:42 -0400

    finished

--- src/pages/bootcamp/[course_slug].jsx
+++ src/pages/bootcamp/[course_slug].jsx
@@ -115,11 +115,11 @@ function CouponTopBar() {
     >
       <Box maxWidth="1280px" margin="auto" display="flex" justifyContent="space-between" alignItems="center">
         <Flex alignItems="center" gap="10px" flexDirection="row" flexWrap="wrap" grow={1} justifyContent="center">
-          <Text color="#FFF" fontSize="15px" fontFamily="inter">
+          <Text color="#FFF" fontSize="18px" fontFamily="inter">
             {t('coupon-bar.headline', { discount })}
           </Text>
           <Flex gap="10px">
-            <Text color="#FFF" fontSize="15px" fontFamily="inter" fontWeight="900">
+            <Text color="#FFF" fontSize="17px" fontFamily="inter" fontWeight="900">
               {t('coupon-bar.ends-in', { time: '' })}
             </Text>
             <Timer
@@ -129,7 +129,7 @@ function CouponTopBar() {
               onFinish={() => setSelfAppliedCoupon(null)}
               color="white"
               background="none"
-              fontSize="15px"
+              fontSize="17px"
               fontFamily="inter"
               fontWeight="900"
             />
