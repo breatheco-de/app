f955e9fe1e611c0905724d8cd85d4df0ba88a11a
Gustavo Márquez <47191295+gustavomm19@users.noreply.github.com>
2024-10-01 11:16:33 +0000

    fix subtasks bug

--- src/pages/syllabus/[cohortSlug]/[lesson]/[lessonSlug]/index.jsx
+++ src/pages/syllabus/[cohortSlug]/[lesson]/[lessonSlug]/index.jsx
@@ -778,7 +778,7 @@ function SyllabusContent() {
                   position="relative"
                   {...getStyles()}
                 >
-                  {Array.isArray(subTasks) && subTasks?.length > 0 && (
+                  {Array.isArray(subTasks) && subTasks?.length > 0 && isAvailableAsSaas && (
                     <SubTasks subTasks={subTasks} assetType={currentAsset?.asset_type} mt="none" />
                   )}
 
