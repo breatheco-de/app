9725dd399288470870ff684564c3907be58306ec
Gustavo Márquez <47191295+gustavomm19@users.noreply.github.com>
2024-10-08 11:16:27 +0000

    refactor markdown parser to separate the toc and the subtasks from the markdown parser

---
+++ src/common/components/MarkDownParser/ArticleMarkdown.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import useModuleHandler from '../../hooks/useModuleHandler';
+import Toc from './toc';
+import ContentHeading from './ContentHeading';
+import SubTasks from './SubTasks';
+import MarkDownParser from './index';
+import OpenWithLearnpackCTA from '../../../js_modules/syllabus/OpenWithLearnpackCTA';
+
+function ArticleMarkdown({
+  content, withToc, frontMatter, titleRightSide, currentTask, currentData,
+  showLineNumbers, showInlineLineNumbers, assetData, alerMessage, isGuidedExperience,
+}) {
+  const { subTasks } = useModuleHandler();
+
+  const assetType = currentData?.asset_type;
+
+  return (
+    <>
+      {!isGuidedExperience && (
+        <ContentHeading
+          titleRightSide={titleRightSide}
+          callToAction={currentData?.interactive && (
+            <OpenWithLearnpackCTA currentAsset={currentData} />
+          )}
+          content={frontMatter}
+          currentData={currentData}
+        />
+      )}
+      {withToc && (
+        <Toc content={content} />
+      )}
+
+      {alerMessage && alerMessage}
+
+      {Array.isArray(subTasks) && subTasks?.length > 0 && (
+        <SubTasks subTasks={subTasks} assetType={assetType} />
+      )}
+
+      <MarkDownParser
+        showLineNumbers={showLineNumbers}
+        showInlineLineNumbers={showInlineLineNumbers}
+        assetData={assetData}
+        currentTask={currentTask}
+        content={content}
+      />
+    </>
+  );
+}
+
+ArticleMarkdown.propTypes = {
+  content: PropTypes.string,
+  withToc: PropTypes.bool,
+  frontMatter: PropTypes.objectOf(PropTypes.oneOfType([PropTypes.object, PropTypes.string, PropTypes.array])),
+  titleRightSide: PropTypes.node,
+  currentTask: PropTypes.objectOf(PropTypes.oneOfType([PropTypes.object, PropTypes.string, PropTypes.array])),
+  currentData: PropTypes.objectOf(PropTypes.oneOfType([PropTypes.object, PropTypes.string, PropTypes.array])),
+  showLineNumbers: PropTypes.bool,
+  showInlineLineNumbers: PropTypes.bool,
+  assetData: PropTypes.objectOf(PropTypes.oneOfType([PropTypes.object])),
+  alerMessage: PropTypes.node,
+  isGuidedExperience: PropTypes.bool,
+};
+ArticleMarkdown.defaultProps = {
+  content: '',
+  withToc: false,
+  frontMatter: {},
+  titleRightSide: null,
+  currentTask: {},
+  currentData: {},
+  showLineNumbers: true,
+  showInlineLineNumbers: true,
+  assetData: null,
+  alerMessage: null,
+  isGuidedExperience: false,
+};
+
+export default ArticleMarkdown;
--- src/js_modules/syllabus/SyllabusMarkdownComponent.jsx
+++ src/js_modules/syllabus/SyllabusMarkdownComponent.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import useTranslation from 'next-translate/useTranslation';
-import MarkDownParser from '../../common/components/MarkDownParser';
+import ArticleMarkdown from '../../common/components/MarkDownParser/ArticleMarkdown';
 import { MDSkeleton } from '../../common/components/Skeleton';
 
 function SyllabusMarkdownComponent({
@@ -12,7 +12,7 @@ function SyllabusMarkdownComponent({
 
   if (ipynbHtmlUrl === null && readme && currentBlankProps?.target !== 'blank') {
     return (
-      <MarkDownParser
+      <ArticleMarkdown
         content={readme.content}
         withToc={lesson?.toLowerCase() === 'read'}
         isGuidedExperience={isGuidedExperience}
@@ -29,7 +29,7 @@ function SyllabusMarkdownComponent({
   }
   if (currentBlankProps?.target === 'blank') {
     return (
-      <MarkDownParser
+      <ArticleMarkdown
         content={blankText}
         withToc={lesson?.toLowerCase() === 'read'}
         isGuidedExperience={isGuidedExperience}
