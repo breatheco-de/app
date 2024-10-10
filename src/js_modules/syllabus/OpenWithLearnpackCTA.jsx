2d558b2d6a3a0b01c7e3c9310c2f54b9352ee86d
Gustavo Márquez <47191295+gustavomm19@users.noreply.github.com>
2024-10-10 10:27:27 +0000

    validate if academy has a vendor configured

--- src/js_modules/syllabus/OpenWithLearnpackCTA.jsx
+++ src/js_modules/syllabus/OpenWithLearnpackCTA.jsx
@@ -13,15 +13,59 @@ import {
 import PropTypes from 'prop-types';
 import useTranslation from 'next-translate/useTranslation';
 import useCohortHandler from '../../common/hooks/useCohortHandler';
-import CallToAction from '../../common/components/CallToAction';
+import useModuleHandler from '../../common/hooks/useModuleHandler';
+import bc from '../../common/services/breathecode';
+import Heading from '../../common/components/Heading';
 import modifyEnv from '../../../modifyEnv';
 import ModalToCloneProject from './ModalToCloneProject';
 import Text from '../../common/components/Text';
 import Icon from '../../common/components/Icon';
 
+function ProvisioningPopover({ openInLearnpackAction, provisioningLinks }) {
+  return (
+    <PopoverContent width="min-content">
+      <PopoverArrow />
+      <PopoverCloseButton />
+      <PopoverHeader>{openInLearnpackAction.title}</PopoverHeader>
+      <PopoverBody display="flex" gridGap="1rem" color="currentColor" flexDirection="column">
+        <Text
+          size="14px"
+          dangerouslySetInnerHTML={{ __html: openInLearnpackAction?.description }}
+          style={{ margin: 0 }}
+        />
+        {provisioningLinks.map((link) => (
+          <Button
+            key={link.text}
+            as="a"
+            display="flex"
+            href={link.link}
+            target={link.isExternalLink ? '_blank' : '_self'}
+            marginY="auto"
+            margin="0"
+            textTransform="uppercase"
+            width="100%"
+            flexDirection="row"
+            gridGap="10px"
+            fontSize="12px"
+            alignItems="center"
+            justifyContent="flex-start"
+            style={{
+              color: 'currentColor',
+            }}
+          >
+            {link.title}
+            <Icon color="currentColor" icon="longArrowRight" />
+          </Button>
+        ))}
+      </PopoverBody>
+    </PopoverContent>
+  );
+}
+
 function OpenWithLearnpackCTA({ currentAsset, variant }) {
-  const { t, lang } = useTranslation('common');
-  const [learnpackActions, setLearnpackActions] = useState([]);
+  const { t } = useTranslation('common');
+  const [vendors, setVendors] = useState([]);
+  const { currentTask } = useModuleHandler();
   const { state } = useCohortHandler();
   const { cohortSession } = state;
   const [showCloneModal, setShowCloneModal] = useState(false);
@@ -41,25 +85,20 @@ function OpenWithLearnpackCTA({ currentAsset, variant }) {
     isExternalLink: true,
   }];
 
-  useEffect(() => {
-    const localhostAction = {
-      text: `${t('learnpack.open-locally')}`,
-      type: 'button',
-      onClick: () => {
-        setShowCloneModal(true);
-      },
-    };
-    const cloudActions = {
-      ...openInLearnpackAction,
-      text: `${openInLearnpackAction.text}${cohortSession?.available_as_saas === false ? ` (${t('learnpack.recommended')})` : ''}`,
-      links: provisioningLinks,
-    };
-    if (cohortSession?.id) {
-      if (!currentAsset?.gitpod) setLearnpackActions([localhostAction]);
-      else if (cohortSession.available_as_saas) setLearnpackActions([localhostAction, cloudActions]);
-      else setLearnpackActions([cloudActions, localhostAction]);
+  const fetchProvisioningVendors = async () => {
+    try {
+      const { data } = await bc.provisioning().academyVendors(cohortSession.academy.id);
+      setVendors(data);
+    } catch (e) {
+      console.log(e);
     }
-  }, [lang, cohortSession?.id, currentAsset?.url]);
+  };
+
+  useEffect(() => {
+    if (cohortSession) {
+      fetchProvisioningVendors();
+    }
+  }, [cohortSession]);
 
   if (variant === 'small') {
     return (
@@ -71,49 +110,16 @@ function OpenWithLearnpackCTA({ currentAsset, variant }) {
               {t('learnpack.choose-open')}
             </Text>
             <Box mt="10px" display="flex" gap="10px" flexDirection={{ base: 'column', md: 'row' }}>
-              <Popover>
-                <PopoverTrigger>
-                  <Button size="sm" padding="4px 8px" fontSize="14px" fontWeight="500" background="gray.200" color="blue.default">
-                    {t('learnpack.open-in-learnpack-button.text')}
-                  </Button>
-                </PopoverTrigger>
-                <PopoverContent width="min-content">
-                  <PopoverArrow />
-                  <PopoverCloseButton />
-                  <PopoverHeader>{openInLearnpackAction.title}</PopoverHeader>
-                  <PopoverBody display="flex" gridGap="1rem" color="currentColor" flexDirection="column">
-                    <Text
-                      size="14px"
-                      dangerouslySetInnerHTML={{ __html: openInLearnpackAction?.description }}
-                      style={{ margin: 0 }}
-                    />
-                    {provisioningLinks.map((link) => (
-                      <Button
-                        key={link.text}
-                        as="a"
-                        display="flex"
-                        href={link.link}
-                        target={link.isExternalLink ? '_blank' : '_self'}
-                        marginY="auto"
-                        margin="0"
-                        textTransform="uppercase"
-                        width="100%"
-                        flexDirection="row"
-                        gridGap="10px"
-                        fontSize="12px"
-                        alignItems="center"
-                        justifyContent="flex-start"
-                        style={{
-                          color: 'currentColor',
-                        }}
-                      >
-                        {link.title}
-                        <Icon color="currentColor" icon="longArrowRight" />
-                      </Button>
-                    ))}
-                  </PopoverBody>
-                </PopoverContent>
-              </Popover>
+              {vendors.length > 0 && (
+                <Popover>
+                  <PopoverTrigger>
+                    <Button size="sm" padding="4px 8px" fontSize="14px" fontWeight="500" background="gray.200" color="blue.default">
+                      {t('learnpack.open-in-learnpack-button.text')}
+                    </Button>
+                  </PopoverTrigger>
+                  <ProvisioningPopover openInLearnpackAction={openInLearnpackAction} provisioningLinks={provisioningLinks} />
+                </Popover>
+              )}
               <Button size="sm" padding="4px 8px" fontSize="14px" fontWeight="500" background="gray.200" color="blue.default" onClick={() => setShowCloneModal(true)}>
                 {t('learnpack.open-locally')}
               </Button>
@@ -127,19 +133,52 @@ function OpenWithLearnpackCTA({ currentAsset, variant }) {
 
   return (
     <>
-      <CallToAction
-        buttonStyle={{
-          color: 'white',
-        }}
-        background="blue.default"
-        reverseButtons={cohortSession?.available_as_saas}
-        margin="12px 0 20px 0px"
-        icon="learnpack"
-        text={t('learnpack.description', { projectName: currentAsset?.title })}
-        width={{ base: '100%', md: 'fit-content' }}
-        buttonsData={learnpackActions}
-        buttonsContainerStyles={{ alignSelf: 'auto' }}
-      />
+      <Box background="blue.1100" borderRadius="11px" padding="16px">
+        <Box display="flex" gap="16px">
+          <Icon icon="learnpack" width="102px" height="102px" />
+          <Box>
+            <Heading size="xsm" mb="15px" color="white">
+              {t('common:learnpack.title')}
+            </Heading>
+            <Text
+              size="l"
+              color="white"
+              dangerouslySetInnerHTML={{ __html: t('common:learnpack.description', { projectName: currentTask?.title }) }}
+            />
+          </Box>
+        </Box>
+        <Box mt="16px" display="flex" gap="16px" flexDirection={{ base: 'column', md: 'row' }}>
+          {vendors.length > 0 && (
+            <Popover>
+              <PopoverTrigger>
+                <Button
+                  borderRadius="3px"
+                  background="white"
+                  color="blue.1000"
+                  display="flex"
+                  gap="16px"
+                  alignItems="center"
+                  fontSize="17px"
+                >
+                  <Icon icon="prov-bridge" width="20px" height="20px" />
+                  {t('common:learnpack.open-in-learnpack-button.text')}
+                </Button>
+              </PopoverTrigger>
+              <ProvisioningPopover openInLearnpackAction={openInLearnpackAction} provisioningLinks={provisioningLinks} />
+            </Popover>
+          )}
+          <Button
+            variant="outline"
+            borderColor="white"
+            color="white"
+            whiteSpace="normal"
+            onClick={() => setShowCloneModal(true)}
+            fontSize="17px"
+          >
+            {t('common:learnpack.open-locally')}
+          </Button>
+        </Box>
+      </Box>
       <ModalToCloneProject currentAsset={currentAsset} isOpen={showCloneModal} onClose={setShowCloneModal} />
     </>
   );
@@ -152,6 +191,11 @@ OpenWithLearnpackCTA.propTypes = {
 OpenWithLearnpackCTA.defaultProps = {
   variant: null,
   currentAsset: null,
+};
+
+ProvisioningPopover.propTypes = {
+  openInLearnpackAction: PropTypes.objectOf(PropTypes.oneOfType([PropTypes.any])).isRequired,
+  provisioningLinks: PropTypes.objectOf(PropTypes.oneOfType([PropTypes.any])).isRequired,
 };
 
 export default OpenWithLearnpackCTA;
