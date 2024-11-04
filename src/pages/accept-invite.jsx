d74ca313ce77c6469caa5d36caa54dc87129e398
lumi-tip <informacionlmdv@gmail.com>
2024-11-04 17:53:26 -0400

    is not creating the user correctly

--- src/pages/accept-invite.jsx
+++ src/pages/accept-invite.jsx
@@ -49,7 +49,10 @@ function AcceptInvite() {
   const [isAccepted, setIsAccepted] = useState(false);
 
   const getInvite = async () => {
-    if (!inviteToken) return;
+    if (!inviteToken) {
+      setIsLoading(false);
+      return;
+    }
     try {
       const resp = await fetch(`${BREATHECODE_HOST}/v1/auth/member/invite/${inviteToken}`, {
         headers: {
@@ -106,6 +109,7 @@ function AcceptInvite() {
           status: 'success',
           duration: 9000,
           isClosable: true,
+          position: 'top',
         });
         router.push('/login');
       }
@@ -164,89 +168,86 @@ function AcceptInvite() {
             </NextChakraLink>
           </Box>
         )}
-      {invite && inviteToken && user && !isAccepted
+      {incorrectUser && user
+        && (
+          <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" mt="2rem" mb="5rem" position="relative" gap="1rem" height="75vh">
+            <Heading fontWeight="500">
+              {t('invalid-user')}
+            </Heading>
+            <NextChakraLink href="/choose-program" variant="buttonDefault">
+              {t('signup:consumables.back-to-dashboard')}
+            </NextChakraLink>
+          </Box>
+        )}
+      {invite && inviteToken && !isAccepted && !incorrectUser
         && (
           <Flex alignItems="center" flexDirection="column" width={['90%', '90%', '50%']} m={['40px 20px', '40px 20px', '40px auto']} maxWidth="1366px">
             <Image width={180} objectFit="cover" src={invite?.academy.logo_url} alt={invite?.academy.name} />
             <Text size="lg" margin="30px" textAlign="center">
-              {
-                incorrectUser
-                  ? (
-                    <Box as="span">
-                      {t('belongs-to-other-user')}
-                      {' '}
-                      <NextChakraLink href="/choose-program" variant="default">{t('signup:consumables.back-to-dashboard')}</NextChakraLink>
-                    </Box>
-                  )
-                  : (
-                    <Box as="span">{t('heading', { name: invite?.academy.name })}</Box>
-                  )
-              }
+              {t('heading', { name: invite?.academy.name })}
             </Text>
-            <Formik
-              initialValues={{
-                first_name: invite?.first_name,
-                last_name: invite?.last_name,
-                email: invite?.email,
-                phone: '',
-                password: '',
-                passwordConfirmation: '',
-              }}
-              onSubmit={putInvite}
-              validationSchema={validationSchema.invite}
-            >
-              {({ isSubmitting }) => (
-                <Form>
-                  <Flex mt="20px">
-                    <FormField name="first_name" label={t('common:first-name')} placeholder={t('common:first-name')} />
-                    <Box ml="20px">
+            {!incorrectUser && (
+              <Formik
+                initialValues={{
+                  first_name: invite?.first_name,
+                  last_name: invite?.last_name,
+                  email: invite?.email,
+                  phone: '',
+                  password: '',
+                  passwordConfirmation: '',
+                }}
+                onSubmit={putInvite}
+                validationSchema={validationSchema.invite}
+              >
+                {({ isSubmitting }) => (
+                  <Form>
+                    <Flex mt="20px" gap="5px">
+                      <FormField name="first_name" label={t('common:first-name')} placeholder={t('common:first-name')} />
                       <FormField name="last_name" label={t('common:last-name')} placeholder={t('common:last-name')} />
-                    </Box>
-                  </Flex>
-
-                  <Flex mt="20px">
-                    <FormField name="phone" label={t('common:phone')} type="tel" placeholder="+123 4567 8900" />
-                    <Text fontSize="12px" color="blue.default" mt="2">
-                      {t('signup:phone-info')}
-                    </Text>
-                  </Flex>
-
-                  <Flex mt="20px">
-                    <FormField name="email" label={t('common:email')} type="email" isReadOnly placeholder="jhon.doe@gmail.com" />
-                  </Flex>
-
-                  <Flex mt="20px">
-                    <FormField name="password" label={t('Choose your password')} type="password" placeholder="***********" />
-                  </Flex>
-
-                  <Flex mt="20px">
-                    <FormField
-                      name="passwordConfirmation"
-                      label={t('Repeat your password')}
-                      type="password"
-                      placeholder="***********"
-                    />
-                  </Flex>
-
-                  <Flex mt="20px" align="center">
-                    <Checkbox me="5px" size="md" spacing="8px" isChecked={isChecked} onChange={() => setIsChecked(!isChecked)} />
-                    <Flex fontSize="10px" gap="10px">
-                      <Text>
-                        {' '}
-                        {t('signup:validators.receive-information')}
-                        {' '}
-                      </Text>
-                      <NextChakraLink variant="default" fontSize="10px" href="/privacy-policy" target="_blank">{t('common:privacy-policy')}</NextChakraLink>
                     </Flex>
-                  </Flex>
-                  {!incorrectUser && (
+
+                    <Flex mt="20px" direction="column">
+                      <FormField name="phone" label={t('common:phone')} type="tel" placeholder="+123 4567 8900" />
+                      <Text fontSize="12px" color="blue.default" mt="2">
+                        {t('signup:phone-info')}
+                      </Text>
+                    </Flex>
+
+                    <Flex mt="20px">
+                      <FormField name="email" label={t('common:email')} type="email" isReadOnly placeholder="jhon.doe@gmail.com" />
+                    </Flex>
+
+                    <Flex mt="20px">
+                      <FormField name="password" label={t('Choose your password')} type="password" placeholder="***********" />
+                    </Flex>
+
+                    <Flex mt="20px">
+                      <FormField
+                        name="passwordConfirmation"
+                        label={t('Repeat your password')}
+                        type="password"
+                        placeholder="***********"
+                      />
+                    </Flex>
+
+                    <Flex mt="20px" align="center">
+                      <Checkbox me="5px" size="md" spacing="8px" isChecked={isChecked} onChange={() => setIsChecked(!isChecked)} />
+                      <Flex fontSize="10px" gap="10px" direction="column">
+                        <Text>
+                          {' '}
+                          {t('signup:validators.receive-information')}
+                          {' '}
+                        </Text>
+                        <NextChakraLink variant="default" fontSize="10px" href="/privacy-policy" target="_blank">{t('common:privacy-policy')}</NextChakraLink>
+                      </Flex>
+                    </Flex>
                     <Button mt="20px" variant="default" width="100%" isLoading={isSubmitting} type="submit" isDisabled={!isChecked}>
                       {t('accept-and-learn')}
                     </Button>
-                  )}
-                </Form>
-              )}
-            </Formik>
+                  </Form>
+                )}
+              </Formik>
+            )}
           </Flex>
         )}
     </>
