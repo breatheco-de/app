1e3cec899010ce288eb0c045f6701fbf0cbace77
Gustavo Márquez <47191295+gustavomm19@users.noreply.github.com>
2024-11-07 11:08:57 +0000

    doing dashboard top bar

---
+++ src/js_modules/chooseProgram/Header.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import useTranslation from 'next-translate/useTranslation';
+
+function Header() {
+  return (
+    <h1>hello</h1>
+  );
+}
+
+export default Header;
