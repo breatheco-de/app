f287f0efc25911bec122864f0986a3e3f2df788e
gustavomm19 <gustavomm199@gmail.com>
2024-11-18 20:51:49 +0000

    update rigobot

--- src/common/context/RigoContext.jsx
+++ src/common/context/RigoContext.jsx
@@ -21,7 +21,7 @@ function RigoProvider({ children }) {
       }}
     >
       <Script
-        src="https://unpkg.com/rigobot-chat-bubble@0.0.55/dist/main.js"
+        src="https://unpkg.com/rigobot-chat-bubble@0.0.57/dist/main.js"
         onLoad={() => {
           const persistedRigoChat = localStorage.getItem('rigo_chat');
           const allowRigo = getQueryString('rigo_chat', persistedRigoChat);
