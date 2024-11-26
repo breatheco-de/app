135ab2c6a0fc0624f2a491af4d43804c6fdc11ab
Tomas Gonzalez <56565994+tommygonzaleza@users.noreply.github.com>
2024-11-26 16:01:25 -0500

    Update RigoContext.jsx

--- src/common/context/RigoContext.jsx
+++ src/common/context/RigoContext.jsx
@@ -23,18 +23,13 @@ function RigoProvider({ children }) {
       <Script
         src="https://unpkg.com/rigobot-chat-bubble@0.0.57/dist/main.js"
         onLoad={() => {
-          const persistedRigoChat = localStorage.getItem('rigo_chat');
-          const allowRigo = getQueryString('rigo_chat', persistedRigoChat);
-          if (allowRigo && allowRigo.toLowerCase() === 'true') {
-            localStorage.setItem('rigo_chat', 'true');
-            window.rigo.init(process.env.RIGOBOT_HASH, {
-              context: '',
-            });
-            window.rigo.show({
-              showBubble: false,
-            });
-            setIsRigoInitialized(true);
-          }
+          window.rigo.init(process.env.RIGOBOT_HASH, {
+            context: '',
+          });
+          window.rigo.show({
+            showBubble: false,
+          });
+          setIsRigoInitialized(true);
         }}
       />
       {children}
