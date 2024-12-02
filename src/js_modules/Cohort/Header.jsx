1cf49218a74e0b3096827e48917b05bdd80bcd57
gustavomm19 <gustavomm199@gmail.com>
2024-12-02 08:35:23 +0000

    add rigobot button

--- src/js_modules/Cohort/Header.jsx
+++ src/js_modules/Cohort/Header.jsx
@@ -9,15 +9,19 @@ import bc from '../../common/services/breathecode';
 import useAuth from '../../common/hooks/useAuth';
 import useCohortHandler from '../../common/hooks/useCohortHandler';
 import useStyle from '../../common/hooks/useStyle';
+import useRigo from '../../common/hooks/useRigo';
 import Heading from '../../common/components/Heading';
 import Text from '../../common/components/Text';
 import Icon from '../../common/components/Icon';
 import { ProfilesSection } from '../../common/components/SupportSidebar/MentoringConsumables';
+import { BREATHECODE_HOST } from '../../utils/variables';
+import { getStorageItem } from '../../utils';
 
 function Header() {
   const { t } = useTranslation('choose-program');
   const router = useRouter();
-  const { user } = useAuth();
+  const { user, isAuthenticatedWithRigobot, conntectToRigobot } = useAuth();
+  const { rigo, isRigoInitialized } = useRigo();
   const { featuredLight, backgroundColor, hexColor } = useStyle();
   const { cohortSession } = useCohortHandler();
   const [mentors, setMentors] = useState([]);
@@ -37,6 +41,31 @@ function Header() {
   useEffect(() => {
     if (cohortSession) fetchServices();
   }, [cohortSession]);
+
+  const hasGithub = user?.github && user.github.username !== '';
+
+  const getRigobotButtonText = () => {
+    if (!hasGithub) return t('common:connect-with-github');
+    if (!isAuthenticatedWithRigobot) return t('common:connect-with-tigobot');
+
+    return t('common:get-help-rigobot');
+  };
+
+  const rigobotMessage = () => {
+    if (!hasGithub) {
+      const accessToken = getStorageItem('accessToken');
+      window.location.href = `${BREATHECODE_HOST}/v1/auth/github/${accessToken}?url=${window.location.href}`;
+    } else if (!isAuthenticatedWithRigobot) {
+      conntectToRigobot();
+    } else {
+      rigo.updateOptions({
+        showBubble: true,
+        highlight: true,
+        collapsed: false,
+        purposeSlug: '4geekscom-public-agent',
+      });
+    }
+  };
 
   return (
     <Container padding="16px 0" maxWidth="none" background={featuredLight}>
@@ -82,13 +111,35 @@ function Header() {
             alignItems="center"
             cursor="pointer"
             _hover={{ textDecoration: 'underline', textDecorationColor: 'blue.default' }}
-            onClick={() => router.push('/workshops')}
+            onClick={() => router.push('/mentorship/schedule')}
           >
             <ProfilesSection size="40px" profiles={mentors} />
             <Text textAlign="center" color={hexColor.blueDefault}>
               {t('common:schedule-mentoring')}
             </Text>
           </Box>
+
+          {isRigoInitialized && (
+            <Box
+              width="145px"
+              height="102px"
+              borderRadius="8px"
+              padding="8px"
+              background={backgroundColor}
+              display="flex"
+              flexDirection="column"
+              justifyContent="space-evenly"
+              alignItems="center"
+              cursor="pointer"
+              _hover={{ textDecoration: 'underline', textDecorationColor: 'blue.default' }}
+              onClick={rigobotMessage}
+            >
+              <Icon icon="rigobot-avatar-tiny" width="42px" height="42px" />
+              <Text textAlign="center" color={hexColor.blueDefault}>
+                {getRigobotButtonText()}
+              </Text>
+            </Box>
+          )}
         </Flex>
       </Flex>
     </Container>
