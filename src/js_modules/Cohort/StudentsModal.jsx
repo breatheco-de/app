59282c6ea37e733533e6a69cc9689a1459651431
gustavomm19 <gustavomm199@gmail.com>
2024-12-18 14:34:04 +0000

    remove comment

--- src/js_modules/Cohort/StudentsModal.jsx
+++ src/js_modules/Cohort/StudentsModal.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react';
 import useTranslation from 'next-translate/useTranslation';
 import PropTypes from 'prop-types';
