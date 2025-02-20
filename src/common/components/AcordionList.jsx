48d029271bc2ce73e75dc917b23678ea4fcac3fc
lumi-tip <informacionlmdv@gmail.com>
2025-02-20 15:42:27 -0400

    start of change

--- src/common/components/AcordionList.jsx
+++ src/common/components/AcordionList.jsx
@@ -1,11 +1,11 @@
-import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box } from '@chakra-ui/react';
+import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, Flex, Text } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
 import Heading from './Heading';
 import Icon from './Icon';
 
 function AcordionList({
   defaultIndex, allowMultiple, list, color, iconColor, paddingButton, titleStyle,
-  highlightColor, containerStyles, unstyled, descriptionStyle, leftIcon, ...rest
+  highlightColor, containerStyles, unstyled, descriptionStyle, leftIcon, expanderText, ...rest
 }) {
   return list?.length > 0 && (
     <Accordion defaultIndex={defaultIndex} allowMultiple={allowMultiple} display="flex" flexDirection="column" gridGap="16px" {...containerStyles}>
@@ -19,6 +19,7 @@ function AcordionList({
                   <Box as="span" flex="1" fontSize="14px" textAlign="left" textTransform="uppercase" {...titleStyle}>
                     {item?.title}
                   </Box>
+                  {expanderText && <Text fontSize="13px">{expanderText}</Text>}
                   <AccordionIcon
                     display={item?.description ? 'block' : 'none'}
                     width="30px"
@@ -31,6 +32,20 @@ function AcordionList({
               <AccordionPanel padding="0 17px 17px" fontSize="14px" {...descriptionStyle}>
                 {item?.description}
               </AccordionPanel>
+              {/*esto es lo que hay que cambiar en lo que tomas me diga donde encontrar esa info*/}
+              <Flex gap="10px" padding="0 18px 18px 18px" alignItems="center">
+                <Icon icon="certificate" color="#0097CD" width="16px" height="16px" />
+                <Text fontSize="12px">Course Certificate</Text>
+
+                <Icon icon="book" color="#0097CD" width="16px" height="16px" />
+                <Text fontSize="12px">4 Readings</Text>
+
+                <Icon icon="strength" color="#0097CD" width="16px" height="16px" />
+                <Text fontSize="12px">1 Exercise</Text>
+
+                <Icon icon="clock" color="#0097CD" width="16px" height="16px" />
+                <Text fontSize="12px">18 Hours</Text>
+              </Flex>
             </>
           )}
         </AccordionItem>
@@ -52,6 +67,7 @@ AcordionList.propTypes = {
   unstyled: PropTypes.bool,
   paddingButton: PropTypes.string,
   leftIcon: PropTypes.string,
+  expanderText: PropTypes.string,
 };
 AcordionList.defaultProps = {
   defaultIndex: null,
@@ -66,5 +82,6 @@ AcordionList.defaultProps = {
   unstyled: false,
   paddingButton: '17px',
   leftIcon: '',
+  expanderText: '',
 };
 export default AcordionList;
