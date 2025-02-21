8cbef9ced961c0fb4aa6d71dcea42b6589b093cb
lumi-tip <informacionlmdv@gmail.com>
2025-02-21 18:29:23 -0400

    create carousel

--- src/common/components/CustomCarousel.jsx
+++ src/common/components/CustomCarousel.jsx
@@ -1,55 +1,71 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
-import { Flex, Box, Text, Image, Badge, IconButton } from '@chakra-ui/react';
+import { Flex, Box, Text, Image, Badge } from '@chakra-ui/react';
 import Icon from './Icon';
 
 function CustomCarousel({ assignmentList }) {
   const [currentSlide, setCurrentSlide] = useState(0);
-  const totalSlides = assignmentList.length;
+  const totalSlides = assignmentList?.length;
+  const slideInterval = useRef(null);
+  const resetTimeout = useRef(null);
 
-  const nextSlide = () => {
+  const startAutoSlide = (delay = 5000) => {
+    clearInterval(slideInterval.current);
+    slideInterval.current = setInterval(() => {
+      setCurrentSlide((prev) => (prev === totalSlides - 1 ? 0 : prev + 1));
+    }, delay);
+  };
+
+  const handleUserInteraction = (callback) => {
+    clearInterval(slideInterval.current);
+    clearTimeout(resetTimeout.current);
+
+    callback();
+
+    resetTimeout.current = setTimeout(() => {
+      startAutoSlide();
+    }, 20000);
+  };
+
+  useEffect(() => {
+    startAutoSlide();
+
+    return () => {
+      clearInterval(slideInterval.current);
+      clearTimeout(resetTimeout.current);
+    };
+  }, [totalSlides]);
+
+  const nextSlide = () => handleUserInteraction(() => {
     setCurrentSlide((prev) => (prev === totalSlides - 1 ? 0 : prev + 1));
-  };
+  });
 
-  const prevSlide = () => {
+  const prevSlide = () => handleUserInteraction(() => {
     setCurrentSlide((prev) => (prev === 0 ? totalSlides - 1 : prev - 1));
-  };
+  });
 
   const difficultyLevel = {
     EASY: 'green',
+    BEGINNER: 'green',
     INTERMEDIATE: 'yellow',
     HARD: 'red',
   };
 
   return (
-    <>
-      <Flex position="relative" width="100%" maxWidth="800px" alignItems="center">
-        <IconButton
-          onClick={prevSlide}
-          aria-label="Previous Slide"
-          icon={<Icon icon="arrowLeft" width="16px" />}
-          position="absolute"
-          left="0"
-          top="50%"
-          transform="translateY(-50%)"
-          bg="white"
-          boxShadow="md"
-          borderRadius="full"
-        />
-
+    <Flex flexDirection="column" gridGap="16px" alignItems="center" position="relative" minHeight="300px" maxWidth="1280px">
+      <Flex width="100%" alignItems="center">
         <Flex
-          flexDirection="row"
+          flexDirection={{ base: 'column', md: 'row' }}
           width="100%"
           border="1px solid #E2E8F0"
           borderRadius="10px"
           overflow="hidden"
           bg="white"
-          boxShadow="md"
           p="16px"
           gridGap="16px"
-          alignItems="center"
+          alignItems="stretch"
         >
-          <Box flex="1" minWidth="250px" maxWidth="400px">
+          <Box flex="1" minWidth="250px" maxWidth="400px" height="250px">
             <Image
               src={assignmentList[currentSlide].preview}
               alt={assignmentList[currentSlide].title}
@@ -60,76 +76,122 @@ function CustomCarousel({ assignmentList }) {
             />
           </Box>
 
-          <Flex flex="1" flexDirection="column" gridGap="10px">
-            <Flex gridGap="8px">
-              {assignmentList[currentSlide].technologies.map((tech) => (
-                <Badge colorScheme="blue">{tech.title}</Badge>
-              ))}
-            </Flex>
-
-            <Text fontSize="20px" fontWeight="bold">{assignmentList[currentSlide].title}</Text>
-            <Text fontSize="14px" color="gray.600">{assignmentList[currentSlide].description}</Text>
-
-            <Flex alignItems="center" gridGap="16px" mt="8px">
-              <Flex alignItems="center" gridGap="8px">
-                <Icon icon="clock" width="16px" />
-                <Text fontSize="14px">{`${assignmentList[currentSlide].duration} hours`}</Text>
+          <Flex flex="1" flexDirection="column" gridGap="10px" justifyContent="space-between">
+            <Flex gridGap="8px" justifyContent="space-between" alignItems="flex-start">
+              <Flex gap="10px">
+                {assignmentList[currentSlide].technologies.map((tech) => (
+                  <>
+                    {tech.icon_url ? (
+                      <Image src={tech.icon_url} width="18px" height="18px" />
+                    ) : (
+                      <Badge borderRadius="10px" px="8px" colorScheme="blue">{tech.title}</Badge>
+                    )}
+                  </>
+                ))}
               </Flex>
-              <Text fontSize="14px" color="gray.500">{new Date(assignmentList[currentSlide].published_at).toLocaleDateString()}</Text>
+
+              <Flex gridGap="16px" alignItems="center">
+                <Badge
+                  borderRadius="20px"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                >
+                  <Icon icon="clock" width="14px" height="14px" color="black" />
+                  <Text fontSize="12px" ml="4px">
+                    {`${assignmentList[currentSlide].duration} hours`}
+                  </Text>
+                </Badge>
+                <Text fontSize="12px" color="gray.500">{new Date(assignmentList[currentSlide].published_at).toLocaleDateString()}</Text>
+              </Flex>
             </Flex>
 
-            <Badge
-              colorScheme={difficultyLevel[assignmentList[currentSlide].difficulty]}
-              mt="8px"
-              alignSelf="flex-start"
-            >
-              {assignmentList[currentSlide].difficulty.charAt(0) + assignmentList[currentSlide].difficulty.slice(1).toLowerCase()}
-            </Badge>
+            <Flex direction="column" mt="16px" gap="16px">
+              <Text fontSize="20px" fontWeight="bold">{assignmentList[currentSlide].title}</Text>
+              <Text fontSize="14px" color="gray.600">{assignmentList[currentSlide].description}</Text>
+            </Flex>
+
+            <Flex alignItems="flex-end" justifyContent="space-between" mt="auto">
+              <Badge
+                colorScheme={difficultyLevel[assignmentList[currentSlide].difficulty]}
+                alignSelf="flex-start"
+                borderRadius="10px"
+                padding="3px 5px"
+              >
+                {assignmentList[currentSlide].difficulty.charAt(0) + assignmentList[currentSlide].difficulty.slice(1).toLowerCase()}
+              </Badge>
+              <Flex gap="10px">
+                <Icon icon="rigobot-avatar-tiny" width="18px" height="18px" />
+                {assignmentList[currentSlide].learnpack_deploy_url && <Icon icon="learnpack" width="18px" height="18px" />}
+                {assignmentList[currentSlide].template_url && <Icon icon="download" width="18px" height="18px" />}
+                {assignmentList[currentSlide].with_video && <Icon icon="video" width="18px" height="18px" />}
+              </Flex>
+            </Flex>
           </Flex>
         </Flex>
+      </Flex>
 
-        <IconButton
-          onClick={nextSlide}
+      <Flex position="relative" width="100%" justifyContent="center">
+        <Icon
+          onClick={currentSlide === 0 ? null : prevSlide}
+          aria-label="Previous Slide"
+          icon="arrowLeft3"
+          position="absolute"
+          left="0"
+          bottom="0"
+          width="30px"
+          height="30px"
+          color={currentSlide === 0 ? '#EBEBEB' : 'black'}
+          cursor={currentSlide === 0 ? undefined : 'pointer'}
+          opacity={currentSlide === 0 ? 0.5 : 1}
+        />
+
+        <Flex height="40px" gridGap="8px" position="relative" alignItems="center">
+          {assignmentList.map((_, index) => (
+            <Box
+              width="10px"
+              height="10px"
+              borderRadius="full"
+              bg={index === currentSlide ? 'blue.500' : 'gray.300'}
+              cursor="pointer"
+              onClick={() => handleUserInteraction(() => setCurrentSlide(index))}
+            />
+          ))}
+        </Flex>
+
+        <Icon
+          onClick={currentSlide === totalSlides - 1 ? null : nextSlide}
           aria-label="Next Slide"
-          icon={<Icon icon="arrowRight" width="16px" />}
+          icon="arrowRight"
           position="absolute"
           right="0"
-          top="50%"
-          transform="translateY(-50%)"
-          bg="white"
-          boxShadow="md"
-          borderRadius="full"
+          bottom="0"
+          width="30px"
+          height="30px"
+          color={currentSlide === totalSlides - 1 ? '#EBEBEB' : 'black'}
+          cursor={currentSlide === totalSlides - 1 ? undefined : 'pointer'}
+          opacity={currentSlide === totalSlides - 1 ? 0.5 : 1}
         />
       </Flex>
-
-      <Flex mt="16px" gridGap="8px">
-        {assignmentList.map((_, index) => (
-          <Box
-            width="10px"
-            height="10px"
-            borderRadius="full"
-            bg={index === currentSlide ? 'blue.500' : 'gray.300'}
-            cursor="pointer"
-            onClick={() => setCurrentSlide(index)}
-          />
-        ))}
-      </Flex>
-    </>
+    </Flex>
   );
 }
 
-// ✅ PropTypes para validar datos
 CustomCarousel.propTypes = {
   assignmentList: PropTypes.arrayOf(PropTypes.shape({
-    title: PropTypes.string.isRequired,
+    title: PropTypes.string,
     preview: PropTypes.string,
-    description: PropTypes.string.isRequired,
-    difficulty: PropTypes.string.isRequired,
-    duration: PropTypes.number.isRequired,
-    published_at: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    difficulty: PropTypes.string,
+    duration: PropTypes.number,
+    published_at: PropTypes.string,
     technologies: PropTypes.arrayOf(PropTypes.shape({
-      title: PropTypes.string.isRequired,
+      title: PropTypes.string,
+      icon_url: PropTypes.string,
     })),
+    learnpack_deploy_url: PropTypes.string,
+    template_url: PropTypes.string,
+    with_video: PropTypes.bool,
   })).isRequired,
 };
 
