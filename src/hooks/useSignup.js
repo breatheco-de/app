0056b2049c03dc1e24830b45aa09e349abf0e9ec
Tomas Gonzalez <56565994+tommygonzaleza@users.noreply.github.com>
2025-06-20 15:24:22 -0400

    Update useSignup.js

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -850,7 +850,7 @@ const useSignup = () => {
 
       if (respPayment?.status_code >= 400) {
         setPaymentStatus('error');
-        console.log("wililililili", respPayment?.detail);
+        console.log('wililililili', respPayment?.detail);
         setDeclinedPayment({
           title: t('transaction-denied'),
           description: respPayment?.detail || t('payment-not-processed'),
