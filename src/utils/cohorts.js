b816457b46808dba898283af6816ec55e959a38c
lumi-tip <informacionlmdv@gmail.com>
2026-09-14 15:51:09 -0400

    change related to avoid teachers hace two micros on the dashboard

--- src/utils/cohorts.js
+++ src/utils/cohorts.js
@@ -404,7 +404,11 @@ export function sortMicroCohortsLikeDashboard(microCohorts, cohortsOrderCsv) {
       return cohortsOrder.indexOf(idA) - cohortsOrder.indexOf(idB);
     });
   }
-  return copy;
+  console.log(copy)
+  const uniqueCopy = copy.filter((item, index, self) => 
+    index === self.findIndex((t) => t.slug === item.slug)
+  );
+  return uniqueCopy;
 }
 
 /**
